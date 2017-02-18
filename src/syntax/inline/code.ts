c862d61628431cab7cbf7a44f9a80a80165d4d11
falsandtru <falsandtru@users.noreply.github.com>
2017-02-18 09:21:46 +0900

    refactor(syntax): cache regexp objects

--- src/syntax/inline/code.ts
+++ src/syntax/inline/code.ts
@@ -8,12 +8,16 @@ import { squash } from './text';
 type SubParsers = [PlainTextParser];
 
 const syntax = /^([\`]+)[\S\s]+?\1/;
+const cache = new Map<string, RegExp>();
 
 export const code: CodeParser = function (source: string): Result<HTMLElement, SubParsers> {
   if (!source.startsWith('`')) return;
   const [whole, keyword] = source.match(syntax) || ['', ''];
   if (!whole) return;
-  const [cs, rest] = loop(compose<SubParsers, Text>([plaintext]), new RegExp(`^${keyword}`))(source.slice(keyword.length)) || [[], ''];
+  if (!cache.has(keyword)) {
+    void cache.set(keyword, new RegExp(`^${keyword}`));
+  }
+  const [cs, rest] = loop(compose<SubParsers, Text>([plaintext]), cache.get(keyword)!)(source.slice(keyword.length)) || [[], ''];
   if (!rest.startsWith(keyword)) return;
   const el = document.createElement('code');
   void el.appendChild(squash(cs));
