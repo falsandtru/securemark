1bcf20cdd49d4e929a5270e30c8da332f33c05e0
falsandtru <falsandtru@users.noreply.github.com>
2017-02-19 14:04:25 +0900

    fix(syntax): fix paragraph parser

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.4.2
+
+- Fix paragraph parser.
+
 ## 0.4.1
 
 - Fix extension parser.
--- src/parser/block/paragraph.ts
+++ src/parser/block/paragraph.ts
@@ -12,9 +12,7 @@ const closer = /^\n\s*?\n/;
 export const paragraph: ParagraphParser = function (source: string): Result<HTMLParagraphElement, SubParsers> {
   if (source.startsWith('\n') || source.match(closer)) return;
   const el = document.createElement('p');
-  const [cs, rest] = loop(compose<SubParsers, HTMLElement | Text>([inline]), closer)(source.trim()) || [[], ''];
+  const [cs, rest] = loop(compose<SubParsers, HTMLElement | Text>([inline]), closer)(source.trim()) || [[document.createTextNode(source.trim())], ''];
   void el.appendChild(squash(cs));
-  return el.childNodes.length === 0
-    ? void 0
-    : consumeBlockEndEmptyLine<HTMLParagraphElement, SubParsers>([el], rest.slice(1));
+  return consumeBlockEndEmptyLine<HTMLParagraphElement, SubParsers>([el], rest.slice(1));
 }
