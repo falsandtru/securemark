a19786a840de4a96b4f3c1997d2994ebe7cd811c
falsandtru <falsandtru@users.noreply.github.com>
2017-02-22 23:58:37 +0900

    refactor(parser): refactor horizontalrule parser

--- src/parser/block/horizontalrule.ts
+++ src/parser/block/horizontalrule.ts
@@ -1,10 +1,10 @@
 ﻿import { Result } from '../../parser';
 import { HorizontalRuleParser, consumeBlockEndEmptyLine } from '../block';
 
-const syntax = /^[ \t]*-[ \t]*-[ \t]*(?:-[ \t]*)+(?:\n|$)/;
+const syntax = /^\s*-\s*-\s*(?:-\s*)+(?:\n|$)/;
 
 export const horizontalrule: HorizontalRuleParser = function (source: string): Result<HTMLHRElement, never> {
-  const [whole] = source.match(syntax) || [''];
+  const [whole] = source.split('\n', 1)[0].match(syntax) || [''];
   if (!whole) return;
-  return consumeBlockEndEmptyLine<HTMLHRElement, never>([document.createElement('hr')], source.slice(whole.length));
+  return consumeBlockEndEmptyLine<HTMLHRElement, never>([document.createElement('hr')], source.slice(whole.length + 1));
 };
