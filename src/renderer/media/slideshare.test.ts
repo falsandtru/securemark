e98ee2ba3b6a69f3e8ef32b7a020e575463c268a
falsandtru <falsandtru@users.noreply.github.com>
2017-08-02 08:42:12 +0900

    test(unit): update tests

--- src/renderer/media/slideshare.test.ts
+++ src/renderer/media/slideshare.test.ts
@@ -3,8 +3,8 @@
 describe('Unit: renderer/media/slideshare', () => {
   describe('slideshare', () => {
     it('result', () => {
-      assert(!slideshare('http://www.slideshare.net/Slideshare/an-overview-to-slideshare-for-business'));
-      assert(slideshare('https://www.slideshare.net/Slideshare/an-overview-to-slideshare-for-business'));
+      assert(!slideshare('http://www.slideshare.net/Slideshare/get-started-with-slide-share'));
+      assert(slideshare('https://www.slideshare.net/Slideshare/get-started-with-slide-share'));
     });
 
   });
