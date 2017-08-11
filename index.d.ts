ceb89a6259807d2a571081de77b95016687031ed
falsandtru <falsandtru@users.noreply.github.com>
2017-08-12 07:02:29 +0900

    refactor(renderer): refactoring

--- index.d.ts
+++ index.d.ts
@@ -10,8 +10,8 @@ export function bind(el: HTMLElement): (source: string) => HTMLElement[];
 export function render(el: HTMLElement, opts?: RenderingOptions): void;
 
 export interface RenderingOptions {
-  code?: (source: HTMLElement) => void;
-  math?: (source: HTMLElement) => void;
+  code?: (target: HTMLElement) => void;
+  math?: (target: HTMLElement) => void;
   media?: {
     twitter?: (url: string) => HTMLElement | void;
     youtube?: (url: string) => HTMLElement | void;
