4675753fb0c08649c8471f68da5f99d1420ad297
falsandtru <falsandtru@users.noreply.github.com>
2017-09-27 19:31:04 +0900

    chore(config): refactoring

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       subdir: function (browser, platform) {
         return browser.toLowerCase().split(' ')[0];
       },
-			includeAllSources: true,
+      includeAllSources: true,
       instrumenters: {
         isparta: require('isparta')
       },
