dab43298836f62613733b037afee95d95a417964
Travis-CI <Travis-CI>
2017-11-05 06:18:08 +0000

    chore(deploy): deploy web contents by Travis-CI

--- assets/js/lib/securemark.js
+++ assets/js/lib/securemark.js
@@ -1,4 +1,4 @@
-/*! securemark v0.34.4 https://github.com/falsandtru/securemark | (c) 2017, falsandtru | (Apache-2.0 AND MPL-2.0) License */
+/*! securemark v0.34.5 https://github.com/falsandtru/securemark | (c) 2017, falsandtru | (Apache-2.0 AND MPL-2.0) License */
 require = function e(t, n, r) {
     function s(o, u) {
         if (!n[o]) {
