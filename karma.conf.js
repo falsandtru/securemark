13249d732758d22928b65e70a474c65d2cd9e87d
falsandtru <falsandtru@users.noreply.github.com>
2017-11-29 16:16:05 +0900

    chore(package): update dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -5,8 +5,8 @@ module.exports = function (config) {
     files: [
       { pattern: 'https://cdn.polyfill.io/v2/polyfill.js?flags=gated&features=default,NodeList.prototype.@@iterator,IntersectionObserver', watched: false, served: false, included: true },
       { pattern: 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', watched: false, served: false, included: true },
-      { pattern: 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.6.0/prism.js', watched: false, served: false, included: true },
-      { pattern: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS_CHTML,Safe', watched: false, served: false, included: true },
+      { pattern: 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.8.4/prism.js', watched: false, served: false, included: true },
+      { pattern: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-AMS_CHTML,Safe', watched: false, served: false, included: true },
       { pattern: 'https://cdnjs.cloudflare.com/ajax/libs/dompurify/1.0.0/purify.js', watched: false, served: false, included: true },
       { pattern: 'node_modules/power-assert/build/power-assert.js', watched: true, served: true, included: true },
       { pattern: 'dist/*.js', watched: true, served: true, included: true }
--- src/renderer/render/code.ts
+++ src/renderer/render/code.ts
@@ -2,5 +2,5 @@
 
 export function code(target: HTMLElement): void {
   assert(target.children.length === 0);
-  void Prism.highlightElement(target, false);
+  void Prism.highlightElement(target, true);
 }
