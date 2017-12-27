9a701a4fd6ae75e9d9da48bb4eea408f69f66bdb
Travis-CI <Travis-CI>
2017-12-27 08:19:33 +0000

    chore(deploy): deploy web contents by Travis-CI

--- assets/js/lib/securemark.js
+++ assets/js/lib/securemark.js
@@ -2574,36 +2574,32 @@ require = function e(t, n, r) {
                 let revision = [];
                 return function* (source) {
                     const rev = revision = [];
-                    const os = pairs.map(([s]) => s);
-                    if (source === os.join(''))
+                    const cs = pairs.map(([s]) => s);
+                    if (source === cs.join(''))
                         return;
                     const ns = segment_1.segment(source);
                     let i = 0;
-                    for (; i < os.length; ++i) {
-                        if (os[i] !== ns[i])
+                    for (; i < cs.length; ++i) {
+                        if (cs[i] !== ns[i])
                             break;
                     }
                     let j = 0;
-                    for (; i + j < os.length && i + j < ns.length; ++j) {
-                        if (os[os.length - j - 1] !== ns[ns.length - j - 1])
+                    for (; i + j < cs.length && i + j < ns.length; ++j) {
+                        if (cs[cs.length - j - 1] !== ns[ns.length - j - 1])
                             break;
                     }
-                    for (const [, es] of pairs.splice(i, pairs.length - j - i)) {
-                        for (const el of es) {
-                            void el.remove();
-                        }
-                    }
+                    void pairs.splice(i, pairs.length - j - i).forEach(([, es]) => void es.forEach(el => void el.remove()));
                     const [, [ref = null] = []] = pairs.slice(i).find(([, [el]]) => !!el) || [];
                     for (const [seg, k] of ns.slice(i, ns.length - j).map((seg, k) => [
                             seg,
                             i + k
                         ])) {
                         const es = parser_1.parse_(seg);
-                        void es.forEach(el => void target.insertBefore(el, ref));
                         void pairs.splice(k, 0, [
                             seg,
                             es
                         ]);
+                        void es.forEach(el => void target.insertBefore(el, ref));
                         yield* es;
                         if (revision !== rev)
                             return;
