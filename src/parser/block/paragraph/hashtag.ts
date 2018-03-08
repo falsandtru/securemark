fe58f94a3491040e399679db3c8e6e1467da5d82
falsandtru <falsandtru@users.noreply.github.com>
2018-03-09 00:18:22 +0900

    refactor(parser): refactoring

--- src/parser/block/paragraph/hashtag.ts
+++ src/parser/block/paragraph/hashtag.ts
@@ -1,7 +1,6 @@
 ﻿import { ParagraphParser } from '../../block';
 import { combine, some } from '../../../combinator';
 import { unescsource } from '../../source/unescapable';
-import { index } from '../../inline';
 import { squash } from '../../squash';
 import { match } from '../../source/validation';
 import { html } from 'typed-dom';
@@ -11,7 +10,6 @@ const closer = /^\s/;
 
 export const hashtag: ParagraphParser.HashtagParser = source => {
   if (!match(source, '#', syntax)) return;
-  if (index(source)) return;
   const line = source.split('\n', 1)[0];
   const [ts = [], rest = undefined] = some(combine<ParagraphParser.HashtagParser>([unescsource]), closer)(line) || [];
   if (rest === undefined) return;
