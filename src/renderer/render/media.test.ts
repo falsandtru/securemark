f0b392d04690001731d25af6bc35a83a4deef19b
falsandtru <falsandtru@users.noreply.github.com>
2018-03-28 08:07:34 +0900

    chore: update dependencies

--- src/renderer/render/media.test.ts
+++ src/renderer/render/media.test.ts
@@ -9,49 +9,49 @@ describe('Unit: renderer/render/media', () => {
   describe('media', () => {
     it('twitter', done => {
       media(image('http://twitter.com/hourenso_u/status/856828123882676225'), {
-        twitter: () => done(false)
+        twitter: () => done(false) || html('div')
       });
       media(image('https://twitter.com/hourenso_u/status/856828123882676225'), {
-        twitter: () => done()
+        twitter: () => done() || html('div')
       });
     });
 
     it('youtube', done => {
       media(image('http://youtu.be/xRF7WIZV4lA'), {
-        youtube: () => done(false)
+        youtube: () => done(false) || html('div')
       });
       media(image('https://youtu.be/xRF7WIZV4lA'), {
-        youtube: () => done()
+        youtube: () => done() || html('div')
       });
       media(image('http://www.youtube.com/watch?v=xRF7WIZV4lA'), {
-        youtube: () => done(false)
+        youtube: () => done(false) || html('div')
       });
       media(image('https://www.youtube.com/watch?v=xRF7WIZV4lA'), {
-        youtube: () => done()
+        youtube: () => done() || html('div')
       });
     });
 
     it('gist', done => {
       media(image('http://gist.github.com/falsandtru/cdf4a19b70012b0d6e3c9e1ec021e557'), {
-        gist: () => done(false)
+        gist: () => done(false) || html('div')
       });
       media(image('https://gist.github.com/falsandtru/cdf4a19b70012b0d6e3c9e1ec021e557'), {
-        gist: () => done()
+        gist: () => done() || html('div')
       });
     });
 
     it('slideshare', done => {
       media(image('http://www.slideshare.net/Slideshare/get-started-with-slide-share'), {
-        slideshare: () => done(false)
+        slideshare: () => done(false) || html('div')
       });
       media(image('https://www.slideshare.net/Slideshare/get-started-with-slide-share'), {
-        slideshare: () => done()
+        slideshare: () => done() || html('div')
       });
     });
 
     it('pdf', done => {
       media(image('http://example.pdf'), {
-        pdf: () => done(false)
+        pdf: () => done(false) || html('div')
       });
       assert(media(image('http://example.com/example.pdf')).querySelector('object')!.getAttribute('type') === 'application/pdf');
       assert(media(image('http://example.com/example.pdf')).querySelector('object')!.typeMustMatch === true);
