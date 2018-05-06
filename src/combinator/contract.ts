fdd1dc6934b2f330e35378ed32658b257c9fd5a6
falsandtru <falsandtru@users.noreply.github.com>
2018-05-06 20:21:01 +0900

    refactor: refactoring

--- src/combinator/contract.ts
+++ src/combinator/contract.ts
@@ -1,7 +1,7 @@
 ﻿import { Parser, Data } from './parser';
 
-export function contract<P extends Parser<any, any>>(pattern: RegExp | string, parser: P, cond: (results: Data<P>[]) => boolean): P;
-export function contract<T, S extends Parser<any, any>[]>(pattern: RegExp | string, parser: Parser<T, S>, cond: (results: T[]) => boolean): Parser<T, S> {
+export function contract<P extends Parser<any, any>>(pattern: RegExp | string, parser: P, cond: (results: Data<P>[], rest: string) => boolean): P;
+export function contract<T, S extends Parser<any, any>[]>(pattern: RegExp | string, parser: Parser<T, S>, cond: (results: T[], rest: string) => boolean): Parser<T, S> {
   return verify(validate(pattern, parser), cond);
 }
 
@@ -21,13 +21,13 @@ export function validate<T, S extends Parser<any, any>[]>(pattern: RegExp | stri
   };
 }
 
-export function verify<P extends Parser<any, any>>(parser: P, cond: (results: Data<P>[]) => boolean): P;
-export function verify<T, S extends Parser<any, any>[]>(parser: Parser<T, S>, cond: (results: T[]) => boolean): Parser<T, S> {
+export function verify<P extends Parser<any, any>>(parser: P, cond: (results: Data<P>[], rest: string) => boolean): P;
+export function verify<T, S extends Parser<any, any>[]>(parser: Parser<T, S>, cond: (results: T[], rest: string) => boolean): Parser<T, S> {
   assert(parser);
   return source => {
     const [rs = [], r = undefined] = parser(source) || [];
     if (r === undefined) return;
-    if (!cond(rs)) return;
+    if (!cond(rs, r)) return;
     assert(source.endsWith(r));
     return r.length < source.length
       ? [rs, r]
