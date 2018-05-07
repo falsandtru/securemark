f2361925241df40b46413d9a5fabf004f0668014
falsandtru <falsandtru@users.noreply.github.com>
2018-05-08 04:50:02 +0900

    refactor: refactoring

--- src/parser/source/block.ts
+++ src/parser/source/block.ts
@@ -1,14 +1,14 @@
 ﻿import { Parser } from '../../combinator';
 import { firstline } from './line';
 
-export function block<P extends Parser<any, any>>(parser: P, separated?: boolean): P;
-export function block<T, S extends Parser<any, any>[]>(parser: Parser<T, S>, separated: boolean = true): Parser<T, S> {
+export function block<P extends Parser<any, any>>(parser: P, separation?: boolean): P;
+export function block<T, S extends Parser<any, any>[]>(parser: Parser<T, S>, separation = true): Parser<T, S> {
   return source => {
     if (source.length === 0) return;
     const result = parser(source);
     if (!result) return;
     const rest = result[1];
-    if (separated && firstline(rest).trim() !== '') return;
+    if (separation && firstline(rest).trim() !== '') return;
     return rest === '' || source[source.length - rest.length - 1] === '\n'
       ? result
       : undefined;
