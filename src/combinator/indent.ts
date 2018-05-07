d5e13a47e4808297741da184588b8d161adaa7cb
falsandtru <falsandtru@users.noreply.github.com>
2018-05-08 00:12:09 +0900

    refactor: refactoring

--- src/combinator/indent.ts
+++ src/combinator/indent.ts
@@ -8,11 +8,10 @@ import { bind } from './bind';
 export function indent<P extends Parser<any, any>>(parser: P): P;
 export function indent<T, S extends Parser<any, any>[]>(parser: Parser<T, S>): Parser<T, S> {
   assert(parser);
-  return bind<string, T, S>(
-    capture(
-      /^\s+/,
-      ([whole], rest) =>
-        some(line(surround(whole, s => [[s.split('\n')[0]], ''], ''), true, true))(whole + rest)),
+  return bind<string, T, S>(capture(
+    /^\s+/,
+    ([whole], rest) =>
+      some(line(surround(whole, s => [[s.split('\n')[0]], ''], ''), true, true))(whole + rest)),
     (rs, rest) => {
       const result = parser(rs.join('\n'));
       return result && result[1] === ''
--- src/parser/block/paragraph/hashtag.ts
+++ src/parser/block/paragraph/hashtag.ts
@@ -5,13 +5,12 @@ import { unescsource } from '../../source/unescapable';
 import { compress } from '../../util';
 import { html } from 'typed-dom';
 
-export const hashtag: ParagraphParser.HashtagParser = line(
-  capture(
-    /^(?=(#+)\S)/,
-    ([, { length: level }], rest) =>
-      fmap(
-        compress(some(union([unescsource]), /^\s/)),
-        ts =>
-          [html('a', { class: 'hashtag', rel: 'noopener', 'data-level': `${level}` }, ts)])
-        (rest)),
-  false);
+export const hashtag: ParagraphParser.HashtagParser = line(capture(
+  /^(?=(#+)\S)/,
+  ([, { length: level }], rest) =>
+    fmap(
+      compress(some(union([unescsource]), /^\s/)),
+      ts =>
+        [html('a', { class: 'hashtag', rel: 'noopener', 'data-level': `${level}` }, ts)])
+      (rest)
+), false);
