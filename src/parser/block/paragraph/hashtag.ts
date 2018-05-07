3a49bd40b7159d4ba3c21635edb23b9079686ccb
falsandtru <falsandtru@users.noreply.github.com>
2018-05-08 00:32:46 +0900

    refactor: refactoring

--- src/parser/block/paragraph/hashtag.ts
+++ src/parser/block/paragraph/hashtag.ts
@@ -1,16 +1,10 @@
 ﻿import { ParagraphParser } from '../../block';
-import { union, some, capture, fmap } from '../../../combinator';
+import { capture } from '../../../combinator';
 import { line } from '../../source/line';
-import { unescsource } from '../../source/unescapable';
-import { compress } from '../../util';
 import { html } from 'typed-dom';
 
 export const hashtag: ParagraphParser.HashtagParser = line(capture(
-  /^(?=(#+)\S)/,
-  ([, { length: level }], rest) =>
-    fmap(
-      compress(some(union([unescsource]), /^\s/)),
-      ts =>
-        [html('a', { class: 'hashtag', rel: 'noopener', 'data-level': `${level}` }, ts)])
-      (rest)
+  /^(#+)\S+/,
+  ([tag, { length: level }], rest) =>
+    [[html('a', { class: 'hashtag', rel: 'noopener', 'data-level': `${level}` }, tag)], rest]
 ), false);
--- src/parser/block/paragraph/reference.ts
+++ src/parser/block/paragraph/reference.ts
@@ -1,16 +1,10 @@
 ﻿import { ParagraphParser } from '../../block';
-import { union, some, capture, fmap, trim } from '../../../combinator';
+import { capture, trim } from '../../../combinator';
 import { line } from '../../source/line';
-import { unescsource } from '../../source/unescapable';
-import { compress } from '../../util';
 import { html } from 'typed-dom';
 
-export const reference: ParagraphParser.ReferenceParser = line(capture(
-  /^(?=(>+)[^>\s])/,
-  ([, { length: level }], rest) =>
-    fmap(
-      compress(trim(some(union([unescsource]), /^\s/))),
-      ts =>
-        [html('a', { class: 'reference', rel: 'noopener', 'data-level': `${level}` }, ts), html('br')])
-      (rest)
+export const reference: ParagraphParser.ReferenceParser = line(trim(capture(
+  /^(>+)[^>\s]\S*$/i,
+  ([ref, { length: level }], rest) =>
+    [[html('a', { class: 'reference', rel: 'noopener', 'data-level': `${level}` }, ref), html('br')], rest])
 ), true, true);
