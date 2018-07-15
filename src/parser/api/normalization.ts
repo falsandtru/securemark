a245f95ac9b19563faaaacd977f33018d081d15d
falsandtru <falsandtru@users.noreply.github.com>
2018-07-15 19:56:16 +0900

    refactor: refactoring

--- src/parser/api/normalization.ts
+++ src/parser/api/normalization.ts
@@ -1,12 +1,10 @@
-const reg = /\r\n|[\x00-\x08\x0B-\x1F\x7F]/g;
-
 export function normalize(source: string): string {
   return source
     .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]?|[\uDC00-\uDFFF]/g, str =>
       str.length === 2
         ? str
         : '')
-    .replace(reg, char => {
+    .replace(/\r\n|[\x00-\x08\x0B-\x1F\x7F]/g, char => {
       assert(!source.match(/^[\n\t]$/));
       switch (char) {
         case '\r':
