bfbf7acb9ad4a93c13e6432b1b63b14bd074adf6
falsandtru <falsandtru@users.noreply.github.com>
2018-08-20 09:12:29 +0900

    refactor: refactoring

--- src/combinator/data/set/sequence.ts
+++ src/combinator/data/set/sequence.ts
@@ -1,24 +1,15 @@
-﻿import { Parser, SubParsers, SubParser, eval, exec, validate } from '../parser';
+﻿import { Parser, SubParsers, SubParser } from '../parser';
+import { union } from './union';
+import { bind } from '../../control/monad/bind';
 import { concat } from 'spica/concat';
 
 export function sequence<P extends Parser<any, any>>(parsers: SubParsers<P>): SubParser<P>;
 export function sequence<T, S extends Parser<T, any>[]>(parsers: S): Parser<T, S> {
   assert(parsers.every(f => !!f));
-  return source => {
-    let rest = source;
-    const results: T[] = [];
-    for (const parser of parsers) {
-      if (rest === '') return;
-      const result = parser(rest);
-      validate(rest, result);
-      if (!result) return;
-      if (exec(result).length >= rest.length) return;
-      void concat(results, eval(result));
-      rest = exec(result);
-    }
-    assert(rest === source || source.slice(1).endsWith(rest));
-    return rest.length < source.length
-      ? [results, rest]
-      : undefined;
-  };
+  return parsers.length <= 1
+    ? union(parsers)
+    : bind(parsers[0], (rs1, rest) =>
+        bind(sequence(parsers.slice(1)), (rs2, rest) =>
+          [concat(rs1, rs2), rest])
+          (rest));
 }
