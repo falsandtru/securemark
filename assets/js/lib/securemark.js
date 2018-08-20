b865e27b1583ad10ddd8081281658337b6d7522a
Travis-CI <Travis-CI>
2018-08-20 10:54:35 +0000

    chore: update web contents

--- assets/js/lib/securemark.js
+++ assets/js/lib/securemark.js
@@ -1,4 +1,4 @@
-/*! securemark v0.78.3 https://github.com/falsandtru/securemark | (c) 2017, falsandtru | (Apache-2.0 AND MPL-2.0) License */
+/*! securemark v0.78.4 https://github.com/falsandtru/securemark | (c) 2017, falsandtru | (Apache-2.0 AND MPL-2.0) License */
 require = function () {
     function r(e, n, t) {
         function o(i, f) {
