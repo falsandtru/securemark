d148effd1b80b28ff75baa353998b770655e292d
falsandtru <falsandtru@users.noreply.github.com>
2018-09-30 08:07:21 +0900

    refactor: refactoring

--- src/renderer/render.ts
+++ src/renderer/render.ts
@@ -5,35 +5,46 @@ import { math } from './render/math';
 
 export function render(target: HTMLElement, opts: RenderingOptions = {}): HTMLElement {
   opts = { code, math, media: {}, ...opts };
-  void [target, ...target.querySelectorAll<HTMLElement>('img.media:not([src])[data-src], a > .media:not(img), pre:not(.quote), .math')]
-    .forEach(target =>
-      void new Promise(() => {
-        switch (true) {
-          case target.matches('.invalid'):
-            return;
-          case target.matches('a > .media:not(img)'):
-            return void target.parentElement!.parentElement!.replaceChild(target, target.parentElement!);
-          case !!opts.media
-            && target.matches('img'): {
-            assert(target.matches('.media:not([src])[data-src]'));
-            const el = media(target as HTMLImageElement, opts.media!);
-            if (!el) return;
-            const scope = target.matches('a > .media') && !el.matches('img')
-              ? target.closest('a')!
-              : target;
-            return void scope.parentElement!.replaceChild(el, scope);
-          }
-          case !!opts.code
-            && target.matches('pre')
-            && target.children.length === 0:
-            return void opts.code!(target);
-          case !!opts.math
-            && target.matches('.math')
-            && target.children.length === 0:
-            return void opts.math!(target);
-          default:
-            return;
-        }
-      }));
-  return target;
+  try {
+    switch (true) {
+      case target.matches('.invalid'):
+        // @ts-ignore
+        return;
+      case target.matches('a > .media:not(img)'):
+        // @ts-ignore
+        return void target.parentElement!.parentElement!.replaceChild(target, target.parentElement!);
+      case !!opts.media
+        && target.matches('img.media:not([src])[data-src]'): {
+        const el = media(target as HTMLImageElement, opts.media!);
+        // @ts-ignore
+        if (!el) return;
+        const scope = target.matches('a > .media') && !el.matches('img')
+          ? target.closest('a')!
+          : target;
+        // @ts-ignore
+        return void scope.parentElement!.replaceChild(el, scope);
+      }
+      case !!opts.code
+        && target.matches('pre:not(.quote)')
+        && target.children.length === 0:
+        // @ts-ignore
+        return void opts.code!(target);
+      case !!opts.math
+        && target.matches('.math')
+        && target.children.length === 0:
+        // @ts-ignore
+        return void opts.math!(target);
+      default:
+        // @ts-ignore
+        return void target.querySelectorAll<HTMLElement>('img.media:not([src])[data-src], a > .media:not(img), pre:not(.quote), .math')
+          .forEach(el =>
+            void render(el, opts));
+    }
+  }
+  catch (reason) {
+    console.error(reason);
+  }
+  finally {
+    return target;
+  }
 }
