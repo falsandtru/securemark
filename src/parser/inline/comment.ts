dfab64c1b97ad6e7897046b40603c470ee2ad939
falsandtru <falsandtru@users.noreply.github.com>
2018-12-10 07:29:03 +0900

    refactor: refactoring

--- src/parser/inline/comment.ts
+++ src/parser/inline/comment.ts
@@ -1,10 +1,8 @@
 ﻿import { CommentParser } from '../inline';
-import { match, verify } from '../../combinator';
+import { match } from '../../combinator';
 import '../source/unescapable';
 import { html } from 'typed-dom';
 
-export const comment: CommentParser = verify(
-  match(
-    /^<(#+)\s+([\s\S]*?\s+)?\1>/,
-    ([, , s = ''], rest) => [[html('sup', { class: 'comment', title: s.trim() })], rest]),
-  ([el]) => el.title.trim() !== '');
+export const comment: CommentParser = match(
+  /^<(#+)\s+(\S[\s\S]*?)\s+\1>/,
+  ([, , title], rest) => [[html('sup', { class: 'comment', title })], rest]);
