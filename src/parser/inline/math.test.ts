f27adae81df7c77383b27f0d22c7078941d46525
falsandtru <falsandtru@users.noreply.github.com>
2018-12-27 05:42:28 +0900

    refactor: refactoring

--- src/parser/inline/math.test.ts
+++ src/parser/inline/math.test.ts
@@ -14,6 +14,7 @@ describe('Unit: parser/inline/math', () => {
       assert.deepStrictEqual(inspect(parser('$a$')), undefined);
       assert.deepStrictEqual(inspect(parser('$0-$1')), undefined);
       assert.deepStrictEqual(inspect(parser('$0 - $1')), undefined);
+      assert.deepStrictEqual(inspect(parser('${}$')), undefined);
       assert.deepStrictEqual(inspect(parser('${ }$')), undefined);
       assert.deepStrictEqual(inspect(parser('${\n}$')), undefined);
       assert.deepStrictEqual(inspect(parser('${a\nb}$')), undefined);
