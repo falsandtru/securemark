bfd0de27ad6c6b1b25a4925b5ae30823df297635
falsandtru <falsandtru@users.noreply.github.com>
2019-01-01 13:58:41 +0900

    refactor: refactoring

--- src/parser/block/paragraph/reference.ts
+++ src/parser/block/paragraph/reference.ts
@@ -1,17 +1,19 @@
 ﻿import { ParagraphParser } from '../../block';
-import { union, some, match, validate, line, rewrite, eval } from '../../../combinator';
+import { union, some, match, line, rewrite, eval } from '../../../combinator';
 import { contentline } from '../../source/line';
 import '../../source/unescapable';
 import { inline } from '../../inline';
 import { html } from 'typed-dom';
 
-export const reference: ParagraphParser.ReferenceParser = line(rewrite(contentline, validate(
-  /^(>+)[^>\s].*/,
+export const reference: ParagraphParser.ReferenceParser = line(rewrite(
+  contentline,
   union<ParagraphParser.ReferenceParser>([
     match(
       /^(>+)[0-9a-z]+\s*$/,
       ([ref, { length: level }], rest) =>
         [[html('a', { class: 'reference', rel: 'noopener', 'data-level': `${level}` }, ref.trim()), html('br')], rest]),
-    () =>
-      [[html('span', { class: 'invalid', 'data-invalid-type': 'syntax' }, eval(some(inline)(`Invalid syntax: Reference: Use lower-case alphanumeric characters in reference syntax.`))), html('br')], ''],
-  ]))));
+    match(
+      /^(>+)(?!>)\S.*$/,
+      () =>
+        [[html('span', { class: 'invalid', 'data-invalid-type': 'syntax' }, eval(some(inline)(`Invalid syntax: Reference: Use lower-case alphanumeric characters in reference syntax.`))), html('br')], '']),
+  ])));
