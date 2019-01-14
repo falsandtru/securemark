b9fb59282aa72957db2b36aee5833841391ba011
falsandtru <falsandtru@users.noreply.github.com>
2019-01-15 00:12:34 +0900

    refactor: refactoring

--- src/combinator/control/manipulation/match.ts
+++ src/combinator/control/manipulation/match.ts
@@ -4,11 +4,11 @@ export function match<P extends Parser<any, any>>(pattern: RegExp, f: (matched: 
 export function match<T, S extends Parser<any, any>[] = []>(pattern: RegExp, f: (matched: string[]) => Parser<T, S>): Parser<T, S> {
   return source => {
     if (source === '') return;
-    const res = source.match(pattern);
-    if (!res) return;
-    assert(source.startsWith(res[0]));
-    const rest = source.slice(res[0].length);
-    const result = f(res)(rest);
+    const param = source.match(pattern);
+    if (!param) return;
+    assert(source.startsWith(param[0]));
+    const rest = source.slice(param[0].length);
+    const result = f(param)(rest);
     if (!result) return;
     assert(rest.endsWith(exec(result)));
     return exec(result).length < source.length && exec(result).length <= rest.length
