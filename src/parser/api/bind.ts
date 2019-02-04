cae3a67f9e8c2f48afda40800a043276fbfe2963
falsandtru <falsandtru@users.noreply.github.com>
2019-02-04 16:49:55 +0900

    refactor: refactoring

--- src/parser/api/bind.ts
+++ src/parser/api/bind.ts
@@ -17,44 +17,59 @@ export function bind(target: DocumentFragment | HTMLElement): (source: string) =
     for (; i < cs.length; ++i) {
       if (cs[i] !== ns[i]) break;
     }
+    assert(i <= cs.length);
     let j = 0;
     for (; i + j < cs.length && i + j < ns.length; ++j) {
       if (cs[cs.length - j - 1] !== ns[ns.length - j - 1]) break;
     }
-    const [, [ref = bottom()] = []] = pairs.slice(pairs.length - j)
-      .find(([, [el = undefined]]) => !!el && !!el.parentNode) || [];
-    for (const [, es] of pairs.splice(i, pairs.length - j - i)) {
-      for (const el of es) {
-        if (el.parentNode !== target) continue;
-        void target.removeChild(el);
-      }
-    }
+    assert(j <= cs.length);
+    assert(i + j <= cs.length);
+    let ref = bottom(i) || target.firstChild;
     let k = i;
     for (const seg of ns.slice(i, ns.length - j)) {
       assert(revision === rev);
       const es = eval(block(seg));
-      void pairs.splice(k, 0, [seg, es]);
+      for (const [, es_] of pairs.splice(k, k < pairs.length - j ? 1 : 0, [seg, es])) {
+        for (const el of es_) {
+          ref = el.nextSibling;
+          if (!el.parentNode) continue;
+          void el.remove();
+        }
+      }
       assert(es.length < 2);
       for (const el of es) {
         assert(revision === rev);
-        void target.insertBefore(el, ref);
+        ref = target.insertBefore(el, ref).nextSibling;
         yield el;
         if (revision !== rev) throw new Error(`Reentered.`);
       }
       void ++k;
     }
+    for (const [, es] of pairs.splice(k, pairs.length - k - j)) {
+      for (const el of es) {
+        if (!el.parentNode) continue;
+        void el.remove();
+      }
+    }
     assert(revision === rev);
     assert(pairs.length === ns.length);
   };
 
-  function bottom(): Node | null {
-    for (let i = pairs.length - 1; i >= 0 && i < pairs.length; --i) {
-      const [, es] = pairs[i];
-      for (let j = es.length - 1; j >= 0; --j) {
-        const el = es[j];
-        if (el.parentNode === target) return el.nextSibling;
+  function bottom(index: number): Node | null {
+    assert(index <= pairs.length);
+    if (pairs.length === 0) return null;
+    if (index === pairs.length) {
+      const el = bottom(pairs.length - 1);
+      return el && el.nextSibling;
+    }
+    for (; index >= 0 && index < pairs.length; --index) {
+      const [, es] = pairs[index];
+      for (let i = es.length - 1; i >= 0; --i) {
+        const el = es[i];
+        if (el.parentNode !== target) continue;
+        return el;
       }
     }
-    return target.firstChild;
+    return null;
   }
 }
