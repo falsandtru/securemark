f32693a4c385d3ef1fd63d4985244911ed5af008
falsandtru <falsandtru@users.noreply.github.com>
2019-02-04 17:13:47 +0900

    refactor: refactoring

--- src/parser/api/bind.ts
+++ src/parser/api/bind.ts
@@ -10,60 +10,60 @@ export function bind(target: DocumentFragment | HTMLElement): (source: string) =
   return function* (source: string): Iterable<HTMLElement> {
     source = normalize(source);
     const rev = revision = Symbol();
-    const cs = pairs.map(([s]) => s);
-    if (source === cs.join('')) return;
-    const ns = segment(source);
-    let i = 0;
-    for (; i < cs.length; ++i) {
-      if (cs[i] !== ns[i]) break;
+    const targetSegments = pairs.map(([seg]) => seg);
+    if (source === targetSegments.join('')) return;
+    const sourceSegments = segment(source);
+    let start = 0;
+    for (; start < targetSegments.length; ++start) {
+      if (targetSegments[start] !== sourceSegments[start]) break;
     }
-    assert(i <= cs.length);
-    let j = 0;
-    for (; i + j < cs.length && i + j < ns.length; ++j) {
-      if (cs[cs.length - j - 1] !== ns[ns.length - j - 1]) break;
+    assert(start <= targetSegments.length);
+    let end = 0;
+    for (; start + end < targetSegments.length && start + end < sourceSegments.length; ++end) {
+      if (targetSegments[targetSegments.length - end - 1] !== sourceSegments[sourceSegments.length - end - 1]) break;
     }
-    assert(j <= cs.length);
-    assert(i + j <= cs.length);
-    let ref = bottom(i) || target.firstChild;
-    let k = i;
-    for (const seg of ns.slice(i, ns.length - j)) {
+    assert(end <= targetSegments.length);
+    assert(start + end <= targetSegments.length);
+    let base = bottom(start) || target.firstChild;
+    let position = start;
+    for (const segment of sourceSegments.slice(start, sourceSegments.length - end)) {
       assert(revision === rev);
-      const es = eval(block(seg));
-      for (const [, es_] of pairs.splice(k, k < pairs.length - j ? 1 : 0, [seg, es])) {
-        for (const el of es_) {
-          ref = el.nextSibling;
+      const elements = eval(block(segment));
+      for (const [, es] of pairs.splice(position, position < pairs.length - end ? 1 : 0, [segment, elements])) {
+        for (const el of es) {
+          base = el.nextSibling;
           if (!el.parentNode) continue;
           void el.remove();
         }
       }
-      assert(es.length < 2);
-      for (const el of es) {
+      assert(elements.length < 2);
+      for (const el of elements) {
         assert(revision === rev);
-        ref = target.insertBefore(el, ref).nextSibling;
+        base = target.insertBefore(el, base).nextSibling;
         yield el;
         if (revision !== rev) throw new Error(`Reentered.`);
       }
-      void ++k;
+      void ++position;
     }
-    for (const [, es] of pairs.splice(k, pairs.length - k - j)) {
+    for (const [, es] of pairs.splice(position, pairs.length - position - end)) {
       for (const el of es) {
         if (!el.parentNode) continue;
         void el.remove();
       }
     }
     assert(revision === rev);
-    assert(pairs.length === ns.length);
+    assert(pairs.length === sourceSegments.length);
   };
 
-  function bottom(index: number): Node | null {
-    assert(index <= pairs.length);
+  function bottom(start: number): Node | null {
+    assert(start <= pairs.length);
     if (pairs.length === 0) return null;
-    if (index === pairs.length) {
+    if (start === pairs.length) {
       const el = bottom(pairs.length - 1);
       return el && el.nextSibling;
     }
-    for (; index >= 0 && index < pairs.length; --index) {
-      const [, es] = pairs[index];
+    for (let i = start; i >= 0 && i < pairs.length; --i) {
+      const [, es] = pairs[i];
       for (let i = es.length - 1; i >= 0; --i) {
         const el = es[i];
         if (el.parentNode !== target) continue;
