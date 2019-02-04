08158eab2675aa6628885d1eada5bad7d2b30daf
falsandtru <falsandtru@users.noreply.github.com>
2019-02-05 06:29:17 +0900

    refactor: refactoring

--- src/parser/api/bind.ts
+++ src/parser/api/bind.ts
@@ -32,7 +32,7 @@ export function bind(target: DocumentFragment | HTMLElement): (source: string) =
       const elements = skip
         ? pairs[position][1]
         : eval(block(segment));
-      for (const [, es] of pairs.splice(position, position < pairs.length - end ? 1 : 0, [segment, elements])) {
+      for (const [, es] of position < pairs.length ? pairs.splice(position, position < pairs.length - end ? 1 : 0, [segment, elements]) : void pairs.push([segment, elements]) || []) {
         for (const el of es) {
           if (!el.parentNode) continue;
           assert(el.parentNode === target);
