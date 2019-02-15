f1ff80dfb8d67f54b70e143c44c000d22f734aea
falsandtru <falsandtru@users.noreply.github.com>
2019-02-16 02:06:56 +0900

    refactor: refactoring

--- src/parser/inline/autolink/hashtag.ts
+++ src/parser/inline/autolink/hashtag.ts
@@ -1,5 +1,5 @@
 ﻿import { AutolinkParser } from '../../inline';
-import { union, some, subline, focus, verify, match, lazy } from '../../../combinator';
+import { union, some, subline, focus, verify, lazy } from '../../../combinator';
 import { unescsource } from '../../source/unescapable';
 import { defrag } from '../../util';
 import { html } from 'typed-dom';
@@ -9,10 +9,10 @@ export const hashtag: AutolinkParser.HashtagParser = lazy(() => verify(
   (_, rest) => !rest.startsWith('#')));
 
 export const hashtag_: AutolinkParser.HashtagParser = subline(union([
-  match(
+  focus(
     /^#((?:[a-zA-Z0-9]|[^\x00-\x7F\s])+)/,
-    ([, tag]) => rest =>
-      [[html('a', { class: 'hashtag', rel: 'noopener' }, `#${tag}`)], rest]),
+    tag =>
+      [[html('a', { class: 'hashtag', rel: 'noopener' }, tag)], '']),
   focus(
     /^[a-zA-Z0-9]*#+/,
     defrag(some(unescsource))),
