faf162f535cc228a23aacb0e84a0591540a5ef07
Travis-CI <Travis-CI>
2019-04-19 21:11:41 +0000

    chore: update web contents

--- assets/js/lib/securemark.js
+++ assets/js/lib/securemark.js
@@ -4074,6 +4074,8 @@ require = function () {
                             const el = media_1.media(target, opts.media);
                             if (!el)
                                 return;
+                            void el.classList.add('media');
+                            void el.setAttribute('data-src', new URL(target.getAttribute('data-src'), window.location.href).href);
                             const scope = target.matches('a > .media') && !el.matches('img') ? target.closest('a') : target;
                             return void scope.parentElement.replaceChild(el, scope);
                         }
@@ -4286,11 +4288,7 @@ require = function () {
                 }, opts);
                 const url = new URL(target.getAttribute('data-src'), window.location.href);
                 const alt = target.getAttribute('alt') || '';
-                const el = undefined || opts.twitter && opts.twitter(url) || opts.youtube && opts.youtube(url) || opts.gist && opts.gist(url) || opts.slideshare && opts.slideshare(url) || opts.pdf && opts.pdf(url) || opts.video && opts.video(url, alt) || opts.audio && opts.audio(url, alt) || opts.image && opts.image(url, alt);
-                if (!el)
-                    return;
-                void el.classList.add('media');
-                return el;
+                return opts.twitter && opts.twitter(url) || opts.youtube && opts.youtube(url) || opts.gist && opts.gist(url) || opts.slideshare && opts.slideshare(url) || opts.pdf && opts.pdf(url) || opts.video && opts.video(url, alt) || opts.audio && opts.audio(url, alt) || opts.image && opts.image(url, alt);
             }
             exports.media = media;
         },
