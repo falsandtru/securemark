734ea6bfa4831facc14dec68269486867d86d835
falsandtru <falsandtru@users.noreply.github.com>
2019-07-26 08:19:51 +0900

    refactor: refactoring

--- src/parser/block/paragraph/mention/address.ts
+++ src/parser/block/paragraph/mention/address.ts
@@ -1,28 +1,26 @@
 import { ParagraphParser } from '../../../block';
-import { union, some, line, rewrite, focus, match, convert, trim, fmap } from '../../../../combinator';
+import { union, line, rewrite, focus, match, convert, trim, fmap } from '../../../../combinator';
 import { link } from '../../../inline';
-import { unescsource } from '../../../source/unescapable';
+import { contentline } from '../../../source/line';
 import { memoize } from '../../../util';
 import { define } from 'typed-dom';
 
-export const address: ParagraphParser.MentionParser.AddressParser = line(
-  fmap(
-    match(
-      /^>+(?=\S+\s*$)/,
-      memoize(([prefix]) => prefix,
-      prefix =>
-        trim(rewrite(
-          union([
-            focus(/^[a-zA-Z0-9]+(?:[/-][a-zA-Z0-9]+)*$/, some(unescsource)),
-            focus(/^https?:\/\/[^/\s]\S*$/, some(unescsource)),
-          ]),
-          convert(source => `[${prefix}]{ ${source} }`, union([link])))))),
-    ([el]) => [
-      define(el,
-        {
-          class: 'address',
-          'data-level': `${el.textContent!.length}`,
-          href: null
-        },
-        `${el.textContent}${el.getAttribute('href')}`)
-    ]));
+export const address: ParagraphParser.MentionParser.AddressParser = line(fmap(match(
+  /^>+(?=\S+\s*$)/,
+  memoize(([prefix]) => prefix,
+  prefix =>
+    trim(rewrite(
+      union([
+        focus(/^[a-zA-Z0-9]+(?:[/-][a-zA-Z0-9]+)*$/, contentline),
+        focus(/^https?:\/\/[^/\s]\S*$/, contentline),
+      ]),
+      convert(source => `[${prefix}]{ ${source} }`, union([link])))))),
+  ([el]) => [
+    define(el,
+      {
+        class: 'address',
+        'data-level': `${el.textContent!.length}`,
+        href: null
+      },
+      `${el.textContent}${el.getAttribute('href')}`)
+  ]));
