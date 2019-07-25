1b267ae1cd350fa9c2ad95b603f275cba812f6cf
falsandtru <falsandtru@users.noreply.github.com>
2019-07-26 04:23:58 +0900

    refactor: refactoring

--- src/parser/block/paragraph/mention/address.ts
+++ src/parser/block/paragraph/mention/address.ts
@@ -1,15 +1,24 @@
 import { ParagraphParser } from '../../../block';
-import { union, line, match, convert, fmap } from '../../../../combinator';
+import { union, some, line, rewrite, focus, match, convert, trim, fmap } from '../../../../combinator';
 import { link } from '../../../inline';
+import { unescsource } from '../../../source/unescapable';
 import { html, define } from 'typed-dom';
+import { memoize } from '../../../util';
 
-export const address: ParagraphParser.MentionParser.AddressParser = line(match(
-  /^(?=((>+)(?:[a-zA-Z0-9]+(?:[/-][a-zA-Z0-9]+)*|https?:\/\/[^/]\S*)\s*$))/,
-  ([, addr, { length: level }]) =>
-    convert(
-      source => `{ ${addr.slice(level).trim()} }${source.slice(addr.length)}`,
-      fmap(union([link]), ([el]) => [
-        html('span',
-          { class: 'address', href: null, 'data-level': `${level}` },
-          [define(el, { href: null }, addr.trim())])
-      ]))));
+export const address: ParagraphParser.MentionParser.AddressParser = line(
+  fmap(
+    match(
+      /^>+(?=\S+\s*$)/,
+      memoize(([prefix]) => prefix,
+      prefix =>
+        trim(rewrite(
+          union([
+            focus(/^[a-zA-Z0-9]+(?:[/-][a-zA-Z0-9]+)*$/, some(unescsource)),
+            focus(/^https?:\/\/[^/\s]\S*$/, some(unescsource)),
+          ]),
+          convert(source => `[${prefix}]{ ${source} }`, union([link])))))),
+    ([el]) => [
+      html('span', { class: 'address', 'data-level': `${el.textContent!.length}` }, [
+        define(el, { href: null }, `${el.textContent}${el.getAttribute('href')}`)
+      ])
+    ]));
