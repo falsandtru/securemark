378d05eadd69ea9f778ccefff15b2cda06484a91
falsandtru <falsandtru@users.noreply.github.com>
2019-07-28 22:10:23 +0900

    refactor: refactoring

--- src/combinator/data/parser/sequence.ts
+++ src/combinator/data/parser/sequence.ts
@@ -5,7 +5,6 @@ export function sequence<P extends Parser<unknown, any>>(parsers: SubParsers<P>)
 export function sequence<T, S extends Parser<T, any>[]>(parsers: S): Parser<T, S> {
   assert(parsers.every(f => !!f));
   return source => {
-    if (parsers.length === 0) return;
     let rest = source;
     const data: T[] = [];
     for (const parser of parsers) {
@@ -16,6 +15,8 @@ export function sequence<T, S extends Parser<T, any>[]>(parsers: S): Parser<T, S
       void concat(data, eval(result));
       rest = exec(result);
     }
-    return [data, rest];
+    return rest.length < source.length
+      ? [data, rest]
+      : undefined;
   };
 }
