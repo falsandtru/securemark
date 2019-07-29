81fb9aaa20b42e5b3e472aa1d2c6fa944690fb18
Travis-CI <Travis-CI>
2019-07-29 08:39:47 +0000

    chore: update web contents

--- assets/js/lib/securemark.js
+++ assets/js/lib/securemark.js
@@ -2601,7 +2601,7 @@ require = function () {
             const inline_1 = _dereq_('../inline');
             const util_1 = _dereq_('../util');
             const typed_dom_1 = _dereq_('typed-dom');
-            exports.annotation = combinator_1.lazy(() => combinator_1.verify(combinator_1.fmap(util_1.trimNodeEnd(combinator_1.surround(/^\n?\(\((?:[^\S\n]*\n)?/, util_1.defrag(combinator_1.some(combinator_1.union([inline_1.inline]), /^\n?\)\)/)), /^\n?\)\)/)), ns => [typed_dom_1.html('sup', { class: 'annotation' }, ns)]), ([el]) => util_1.hasTightText(el) && !util_1.hasMedia(el) && !util_1.hasAnnotationOrAuthority(el)));
+            exports.annotation = combinator_1.lazy(() => combinator_1.verify(combinator_1.fmap(util_1.trimNodeEnd(combinator_1.surround('((', util_1.defrag(combinator_1.some(combinator_1.union([inline_1.inline]), '))')), '))')), ns => [typed_dom_1.html('sup', { class: 'annotation' }, ns)]), ([el]) => util_1.hasTightText(el) && !util_1.hasMedia(el) && !util_1.hasAnnotationOrAuthority(el)));
         },
         {
             '../../combinator': 20,
