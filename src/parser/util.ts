bd4e364d446afa0b2ed8a03a6ba4c0ae19237b31
falsandtru <falsandtru@users.noreply.github.com>
2019-08-14 23:38:30 +0900

    refactor: refactoring

--- src/parser/util.ts
+++ src/parser/util.ts
@@ -127,7 +127,8 @@ export function stringify(nodes: Node[]): string {
 
 export function suppress<T extends HTMLElement | DocumentFragment>(target: T): T {
   for (const el of target.querySelectorAll('[id], a.index, a.label, .annotation > a, .authority > a, li[id] > sup:last-child > a')) {
-    if (el.tagName === 'A' ? !el.id && !el.hasAttribute('href') : !el.id) continue;
+    if (el.tagName === 'A' && !el.id && !el.hasAttribute('href')) continue;
+    assert(el.matches('[id], a[href]'));
     void define(el, { id: null, href: null });
   }
   return target;
