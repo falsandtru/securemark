d9844dcb27fcbec4a5996375e43b7c1de22867f6
falsandtru <falsandtru@users.noreply.github.com>
2019-08-23 23:45:40 +0900

    refactor: refactoring

--- src/util/figure.ts
+++ src/util/figure.ts
@@ -32,28 +32,30 @@ export function figure(source: DocumentFragment | HTMLElement | ShadowRoot): voi
     assert(def.matches('figure') || number.endsWith('.0'));
     if (number.split('.').pop() === '0') {
       assert(isFixed(label));
-      if (number === '0') {
-        number = `0${'.0'.repeat(bases.length - 1)}`;
-      }
-      else if (number.startsWith('0.') && number.endsWith('.0')) {
-        number = bases.slice()
-          .reduce((idx, _, i, bases) => {
-            i === idx.length
-              ? bases.length = i
-              : idx[i] = +idx[i] > +bases[i]
-                ? idx[i]
-                : +idx[i] === 0
-                  ? bases[i]
-                  : `${+bases[i] + 1}`;
-            return idx;
-          }, number.split('.'))
-          .join('.');
+      switch (true) {
+        case number === '0':
+          number = `0${'.0'.repeat(bases.length - 1)}`;
+          break;
+        case number.startsWith('0.'):
+          assert(number.endsWith('.0'));
+          number = bases.slice()
+            .reduce((idx, _, i, bases) => {
+              i === idx.length
+                ? bases.length = i
+                : idx[i] = +idx[i] > +bases[i]
+                  ? idx[i]
+                  : +idx[i] === 0
+                    ? bases[i]
+                    : `${+bases[i] + 1}`;
+              return idx;
+            }, number.split('.'))
+            .join('.');
+          break;
       }
       base = number;
       bases = base.split('.');
       void numbers.clear();
-      if (def.tagName !== 'FIGURE') continue;
-      assert(!void def.setAttribute('data-number', number));
+      assert(def.tagName !== 'FIGURE' || !void def.setAttribute('data-number', number));
       continue;
     }
     assert(def.matches('figure:not([style])'));
