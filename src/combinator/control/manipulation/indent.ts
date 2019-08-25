848b2fb22192d3ff7e75e432b158f895e0f4dd12
falsandtru <falsandtru@users.noreply.github.com>
2019-08-26 05:53:06 +0900

    refactor: refactoring

--- src/combinator/control/manipulation/indent.ts
+++ src/combinator/control/manipulation/indent.ts
@@ -1,10 +1,10 @@
 import { Parser, eval, exec } from '../../data/parser';
 import { some } from '../../data/parser/some';
+import { line, firstline } from '../constraint/line';
+import { rewrite } from '../constraint/scope';
 import { bind } from '../monad/bind';
 import { match } from './match';
 import { surround } from './surround';
-import { line, firstline } from '../constraint/line';
-import { rewrite } from '../constraint/scope';
 
 export function indent<P extends Parser<unknown, any>>(parser: P): P;
 export function indent<T, S extends Parser<unknown, any>[]>(parser: Parser<T, S>): Parser<T, S> {
--- src/parser/block/paragraph/mention.ts
+++ src/parser/block/paragraph/mention.ts
@@ -1,8 +1,7 @@
 import { ParagraphParser } from '../../block';
-import { subsequence, some, block, bind } from '../../../combinator';
+import { subsequence, some, block, firstline, bind } from '../../../combinator';
 import { address } from './mention/address';
 import { quote } from './mention/quote';
-import { firstline } from '../../../combinator';
 import { html } from 'typed-dom';
 
 export const mention: ParagraphParser.MentionParser = block(bind(
