c494554572aa97b3315084e17515a8cac584de23
falsandtru <falsandtru@users.noreply.github.com>
2019-10-28 02:08:57 +0900

    refactor: refactoring

--- src/parser/block/paragraph/mention/quotation.ts
+++ src/parser/block/paragraph/mention/quotation.ts
@@ -8,10 +8,10 @@ import { html } from 'typed-dom';
 export const quotation: ParagraphParser.MentionParser.QuotationParser = lazy(() => block(fmap(
   union([
     rewrite(
-      some(validate(/^(?=>+(?:\s|$))/, contentline)),
+      some(validate(/^>+(?:\s|$)/, contentline)),
       convert(source => source.replace(/\n$/, ''), defrag(some(autolink)))),
     rewrite(
-      some(validate(/^>/, contentline)),
+      some(validate(/^>+/, contentline)),
       convert(source => source.replace(/\n$/, ''), defrag(some(autolink)))),
   ]),
   ns => [html('span', { class: 'quotation' }, ns)]),
