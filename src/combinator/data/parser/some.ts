ae4a5a054a08bb711fcfa1f0cc311269e8328f28
falsandtru <falsandtru@users.noreply.github.com>
2019-10-28 12:00:30 +0900

    refactor: refactoring

--- src/combinator/data/parser/some.ts
+++ src/combinator/data/parser/some.ts
@@ -1,11 +1,15 @@
 import { Parser, eval, exec, check } from '../parser';
 import { concat } from 'spica/concat';
 
+const memsize = 1000;
+
 export function some<P extends Parser<unknown, any>>(parser: P, until?: string | RegExp): P;
 export function some<T, S extends Parser<unknown, any>[]>(parser: Parser<T, S>, until?: string | RegExp): Parser<T, S> {
   assert(parser);
   assert(until instanceof RegExp ? !until.global && until.source.startsWith('^') : true);
+  let memory = '';
   return source => {
+    if (source === memory) return;
     let rest = source;
     const data: T[] = [];
     while (true) {
@@ -17,6 +21,7 @@ export function some<T, S extends Parser<unknown, any>[]>(parser: Parser<T, S>, 
       void concat(data, eval(result));
       rest = exec(result);
     }
+    memory = source.length < memsize && rest || memory;
     assert(rest.length <= source.length);
     return rest.length < source.length
       ? [data, rest]
