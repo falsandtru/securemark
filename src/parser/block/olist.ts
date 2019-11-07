3200a3207d3f7d27081908d8620d787e8454d2c6
falsandtru <falsandtru@users.noreply.github.com>
2019-11-08 06:40:15 +0900

    refactor: refactoring

--- src/parser/block/olist.ts
+++ src/parser/block/olist.ts
@@ -1,35 +1,43 @@
 import { OListParser } from '../block';
-import { union, inits, some, block, line, focus, match, memoize, surround, convert, indent, trim, configure, fmap } from '../../combinator';
+import { union, inits, some, block, line, validate, focus, surround, convert, indent, trim, configure, lazy, fmap } from '../../combinator';
 import { ulist_, fillFirstLine } from './ulist';
 import { ilist_ } from './ilist';
 import { inline } from '../inline';
 import { unescsource } from '../source';
 import { defrag } from '../util';
-import { memoize as memorize } from 'spica/memoization';
-import { html } from 'typed-dom';
+import { html, define } from 'typed-dom';
 
-const opener = memorize<string, RegExp>(pattern => new RegExp(`^${pattern}(?:\\.\\s|\\.?(?=\\n|$))`));
-
-export const olist: OListParser = block(match(
+export const olist: OListParser = lazy(() => block(fmap(validate(
   /^(?=([0-9]+|[a-z]+|[A-Z]+)\.(?:[^\S\n]|\n[^\S\n]*\S))/,
-  memoize(([, index]) => index,
-  index =>
+  configure({ syntax: { inline: { media: false } } },
+  some(union([
     fmap(
-      configure({ syntax: { inline: { media: false } } },
-      some(union([
-        fmap(
-          inits([
-            line(inits([
-              focus(
-                opener(pattern(type(index))),
-                defrag(trim(surround('', some(unescsource, /^[.\n]/), /^\.?/)))),
-              defrag(trim(some(inline))),
-            ])),
-            indent(union([ulist_, olist_, ilist_]))
-          ]),
-          ([{ textContent: index }, ...ns]) => [html('li', { value: type(index!) === '1' ? format(index!) : undefined }, fillFirstLine(ns))]),
-      ]))),
-      es => [html('ol', { type: type(index), start: type(index) === '1' ? format(index) : undefined }, es)]))));
+      inits([
+        line(inits([
+          focus(
+            /^(?:[0-9]+|[a-z]+|[A-Z]+)(?:\.\s|\.?(?=\n|$))/,
+            defrag(trim(surround('', some(unescsource, /^[.\n]/), /^\.?/)))),
+          defrag(trim(some(inline))),
+        ])),
+        indent(union([ulist_, olist_, ilist_]))
+      ]),
+      ([{ textContent: index }, ...ns]) => [html('li', { value: format(index!), 'data-type': type(index) }, fillFirstLine(ns))]),
+  ])))),
+  es => {
+    const ty = es[0].getAttribute('data-type');
+    return [
+      html('ol',
+        {
+          type: ty,
+          start: ty === '1' ? es[0].getAttribute('value') : undefined
+        },
+        es.map(el =>
+          define(el, {
+            value: ty === '1' ? undefined : null,
+            'data-type': null
+          })))
+    ];
+  })));
 
 export const olist_: OListParser = convert(
   source => source.replace(/^([0-9]+|[A-Z]+|[a-z]+)\.?(?=\n|$)/, `$1. `),
@@ -37,7 +45,8 @@ export const olist_: OListParser = convert(
 
 type IndexType = undefined | '1' | 'a' | 'A';
 
-function type(index: string): IndexType {
+function type(index: string | null): IndexType {
+  if (index === null) return;
   switch (true) {
     case +index === 0:
       return undefined;
@@ -49,19 +58,6 @@ function type(index: string): IndexType {
       return 'A';
     default:
       throw new Error(`${index}`);
-  }
-}
-
-function pattern(type: IndexType): string {
-  switch (type) {
-    case undefined:
-      return `(?:${pattern('1')}|${pattern('a')}|${pattern('A')})`;
-    case '1':
-      return '(?:[0-9]+)';
-    case 'a':
-      return '(?:[a-z]+|0)';
-    case 'A':
-      return '(?:[A-Z]+|0)';
   }
 }
 
