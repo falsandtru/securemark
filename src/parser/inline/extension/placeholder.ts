63daa1b88b6ca7f11dea65ecd3b2a9a865560512
falsandtru <falsandtru@users.noreply.github.com>
2019-12-01 16:09:04 +0900

    refactor: refactoring

--- src/parser/inline/extension/placeholder.ts
+++ src/parser/inline/extension/placeholder.ts
@@ -1,13 +1,13 @@
 import { ExtensionParser, inline } from '../../inline';
-import { union, some, subline, validate, surround, lazy, fmap, eval } from '../../../combinator';
+import { union, some, subline, surround, lazy, fmap, eval } from '../../../combinator';
 import { defrag } from '../../util';
 import { html } from 'typed-dom';
 
 // Already used symbols: !@$&*<
 export const placeholder: ExtensionParser.PlaceholderParser = lazy(() => subline(fmap(
   surround(
-    '[',
-    validate(/^[:^](?!])/, defrag(some(union([inline]), /^\\?\n|^]/))),
+    /^\[[:^]/,
+    defrag(some(union([inline]), /^\\?\n|^]/)),
     ']'),
   () =>
     [html('span', { class: 'invalid', 'data-invalid-syntax': 'extension', 'data-invalid-type': 'syntax' }, eval(some(inline)(`Invalid syntax: Extension: Invalid flag.`, {}, {})))])));
