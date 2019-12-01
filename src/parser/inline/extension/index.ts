448b71517c8a44bfae6b5f7b981791e73cf99762
falsandtru <falsandtru@users.noreply.github.com>
2019-12-01 14:35:06 +0900

    refactor: refactoring

--- src/parser/inline/extension/index.ts
+++ src/parser/inline/extension/index.ts
@@ -1,19 +1,14 @@
 import { ExtensionParser, inline } from '../../inline';
-import { union, some, subline, rewrite, verify, surround, convert, configure, lazy, fmap } from '../../../combinator';
+import { union, some, subline, rewrite, surround, convert, configure, lazy, fmap } from '../../../combinator';
 import { link } from '../link';
 import { indexee } from './indexee';
-import { hasTightText } from '../../util';
 import { define } from 'typed-dom';
 
-export const index: ExtensionParser.IndexParser = lazy(() => subline(fmap(indexee(verify(
+export const index: ExtensionParser.IndexParser = lazy(() => subline(fmap(
   configure({ syntax: { inline: { link: undefined, media: false } } },
-  surround(
-    '[#',
-    rewrite(
-      some(inline, /^\\?\n|^]/),
-      convert(
-        query => `[${query}]{#}`,
-        union([link]))),
-    ']')),
-  ([el]) => hasTightText(el))),
+  rewrite(
+    surround('[#', some(inline, /^\\?\n|^]/), ']'),
+    convert(
+      query => `[${query.slice(2, -1)}]{#}`,
+      indexee(union([link]))))),
   ([el]) => [define(el, { id: null, class: 'index', href: `#${el.id}` })])));
