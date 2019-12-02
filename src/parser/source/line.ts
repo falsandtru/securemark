2df271a5a15535bb5b21e79fc9dfc24065787cc6
falsandtru <falsandtru@users.noreply.github.com>
2019-12-02 11:19:16 +0900

    refactor: refactoring

--- src/parser/source/line.ts
+++ src/parser/source/line.ts
@@ -4,6 +4,6 @@ import { line } from '../../combinator';
 export const anyline: AnyLineParser = line(() => [[], ''], false);
 export const emptyline: EmptyLineParser = line(s => s.trim() === '' ? [[], ''] : undefined, false);
 
-const invisible = /^(?:\\?\s)*$/;
+const invisible = /^(?:\\?\s)*\\?$/;
 export const blankline: BlankLineParser = line(s => invisible.test(s) ? [[], ''] : undefined, false);
 export const contentline: ContentLineParser = line(s => !invisible.test(s) ? [[], ''] : undefined, false);
