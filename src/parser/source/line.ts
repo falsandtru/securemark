01b5aafdb20a529788dc92f5ff7304b564da6a95
falsandtru <falsandtru@users.noreply.github.com>
2019-12-09 07:31:28 +0900

    refactor: refactoring

--- src/parser/source/line.ts
+++ src/parser/source/line.ts
@@ -2,5 +2,11 @@ import { AnyLineParser, EmptyLineParser, ContentLineParser } from '../source';
 import { line } from '../../combinator';
 
 export const anyline: AnyLineParser = line(() => [[], ''], false);
-export const emptyline: EmptyLineParser = line(s => s.trim() === '' ? [[], ''] : undefined, false);
-export const contentline: ContentLineParser = line(s => s.trim() !== '' ? [[], ''] : undefined, false);
+export const emptyline: EmptyLineParser = line(s => isEmpty(s) ? [[], ''] : undefined, false);
+export const contentline: ContentLineParser = line(s => !isEmpty(s) ? [[], ''] : undefined, false);
+
+function isEmpty(line: string): boolean {
+  return line === ''
+      || line === '\n'
+      || line.trim() === '';
+}
