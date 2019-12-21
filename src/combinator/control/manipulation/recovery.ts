ef0a25a2d9dd7f1af6d6fcb14da0857f66b83794
falsandtru <falsandtru@users.noreply.github.com>
2019-12-21 11:30:55 +0900

    refactor: refactoring

--- src/combinator/control/manipulation/recovery.ts
+++ src/combinator/control/manipulation/recovery.ts
@@ -1,7 +1,7 @@
 import { Parser, Result, Data, SubParsers, Config } from '../../data/parser';
 
 export function recover<P extends Parser<unknown>>(parser: P, fallback: (source: string, config: Config<P>, err: unknown) => Result<Data<P>, SubParsers<P>>): P;
-export function recover<T, D extends Parser<unknown>[]>(parser: Parser<T, D>, fallback: (source: string, config: Config<Parser<T, D>>, err: unknown) => Result<Data<Parser<T, D>>, SubParsers<Parser<T, D>>>): Parser<T, D> {
+export function recover<T, D extends Parser<unknown>[]>(parser: Parser<T, D>, fallback: (source: string, config: object, err: unknown) => Result<Data<Parser<T, D>>, SubParsers<Parser<T, D>>>): Parser<T, D> {
   return (source, config) => {
     try {
       return parser(source, config);
