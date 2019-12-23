bf64ee703189f1b7a053029d419c76a70fd0649b
Travis-CI <Travis-CI>
2019-12-23 15:23:41 +0000

    chore: update web contents

--- assets/js/lib/securemark.js
+++ assets/js/lib/securemark.js
@@ -2047,8 +2047,7 @@ require = function () {
                                 yield el;
                             }
                         }
-                        void ++index;
-                        if (skip)
+                        if (skip && ++index)
                             continue;
                         for (const el of elements) {
                             base = (base === null || base === void 0 ? void 0 : base.parentNode) === target ? base : next(index);
@@ -2057,6 +2056,7 @@ require = function () {
                             if (revision !== rev)
                                 throw new Error(`Abort by reentering.`);
                         }
+                        void ++index;
                     }
                     for (const [, es] of pairs.splice(index, pairs.length - index - end)) {
                         for (const el of es) {
