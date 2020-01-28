425ce5d8166d2e9fb88715e7bdbe10a13790aaf0
falsandtru <falsandtru@users.noreply.github.com>
2020-01-29 07:14:41 +0900

    refactor: refactoring

--- src/combinator/control/manipulation/config.ts
+++ src/combinator/control/manipulation/config.ts
@@ -1,7 +1,7 @@
 import { WeakMap } from 'spica/global';
-import { ObjectFreeze, ObjectKeys } from 'spica/alias';
 import { Parser, Config } from '../../data/parser';
 import { extend } from 'spica/assign';
+import { memoize } from 'spica/memoize';
 
 export function guard<P extends Parser<object>>(f: (config: Config<P>) => boolean, parser: P): P;
 export function guard<T extends object, D extends Parser<unknown, any, S, C>[], S extends object, C extends object>(f: (config: C) => boolean, parser: Parser<T, D, S, C>): Parser<T, D, S, C> {
@@ -11,15 +11,9 @@ export function guard<T extends object, D extends Parser<unknown, any, S, C>[], 
       : void 0;
 }
 
-const singleton = ObjectFreeze({});
-
-export function configure<P extends Parser<object>>(config: Config<P>, parser: P): P;
+export function configure<P extends Parser<object>>(config: Partial<Config<P>>, parser: P): P;
 export function configure<T extends object, D extends Parser<unknown, any, S, C>[], S extends object, C extends object>(config: C, parser: Parser<T, D, S, C>): Parser<T, D, S, C> {
-  const memory = new WeakMap<C, C>();
-  return (source, base) => {
-    base = !memory.has(base) && ObjectKeys(base).length === 0
-      ? singleton as C
-      : base;
-    return parser(source, memory.get(base) || memory.set(base, extend<C>({}, base, config)).get(base)!);
-  };
+  const ex = memoize<C, C>(base => extend({}, base, config), new WeakMap());
+  return (source, base) =>
+    parser(source, ex(base));
 }
