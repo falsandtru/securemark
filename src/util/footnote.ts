65b16cb6940164cfc9c401db45060026805c3279
falsandtru <falsandtru@users.noreply.github.com>
2020-02-03 09:04:59 +0900

    Refactoring

--- src/util/footnote.ts
+++ src/util/footnote.ts
@@ -16,33 +16,45 @@ const identify = memoize<HTMLElement, string>(
   ref => ref.getAttribute('data-alias') || ref.innerHTML,
   new WeakMap());
 
-function build(group: string, marker: (index: number) => string): (target: DocumentFragment | HTMLElement | ShadowRoot, footnote: HTMLOListElement) => Generator<HTMLAnchorElement | HTMLLIElement, undefined, undefined> {
-  assert(group.match(/^[a-z]+$/));
-  const contents = new WeakMap<HTMLElement, Node[]>();
+function build(syntax: string, marker: (index: number) => string): (target: DocumentFragment | HTMLElement | ShadowRoot, footnote: HTMLOListElement) => Generator<HTMLAnchorElement | HTMLLIElement, undefined, undefined> {
+  assert(syntax.match(/^[a-z]+$/));
+  const contentify = memoize<HTMLElement, DocumentFragment>(
+    ref => frag(ref.childNodes),
+    new WeakMap());
   return function* (target: DocumentFragment | HTMLElement | ShadowRoot, footnote: HTMLOListElement): Generator<HTMLAnchorElement | HTMLLIElement, undefined, undefined> {
     const check = context(target);
     const defs = new Map<string, HTMLLIElement>();
     const titles = new Map<string, string>();
     let count = 0;
-    for (const ref of target.querySelectorAll<HTMLElement>(`.${group}`)) {
+    for (const ref of target.querySelectorAll<HTMLElement>(`.${syntax}`)) {
       if (!check(ref)) continue;
       void ++count;
       const identifier = identify(ref);
-      const refIndex = count;
-      const refId = ref.id || `${group}:ref:${count}`;
-      const def = defs.get(identifier);
-      const defIndex = def
-        ? +def.id.slice(def.id.lastIndexOf(':') + 1)
-        : defs.size + 1;
-      const defId = def
-        ? def.id
-        : `${group}:def:${defIndex}`;
       const title = ref.classList.contains('invalid')
         ? void 0
         : titles.get(identifier) || ref.title || text(ref);
-      const refChild = ref.firstChild as HTMLAnchorElement | null;
-      !contents.has(ref) && void contents.set(ref, [...ref.childNodes]);
       title && !titles.has(title) && void titles.set(identifier, title);
+      const content = contentify(ref);
+      const refIndex = count;
+      const refId = ref.id || `${syntax}:ref:${count}`;
+      const def = defs.has(identifier)
+        ? defs.get(identifier)!
+        : defs.set(identifier, html('li',
+            { id: `${syntax}:def:${defs.size + 1}`, class: 'footnote' },
+            [content.cloneNode(true), html('sup', [])]))
+            .get(identifier)!;
+      assert(def.lastChild);
+      if (title && def.childNodes.length === 1) {
+        assert(content.childNodes.length > 0);
+        void def.insertBefore(
+          content.cloneNode(true),
+          def.lastChild);
+        assert(def.childNodes.length > 1);
+      }
+      const defIndex = +def.id.slice(def.id.lastIndexOf(':') + 1);
+      const defId = def.id;
+      const refChild = ref.firstChild as HTMLAnchorElement | null;
+      assert(refChild instanceof HTMLAnchorElement || !refChild);
       yield define(ref,
         title
           ? { id: refId, title }
@@ -50,7 +62,7 @@ function build(group: string, marker: (index: number) => string): (target: Docum
               class: ref.classList.contains('invalid')
                 ? undefined
                 : [...ref.classList, 'invalid'].join(' '),
-              'data-invalid-syntax': group,
+              'data-invalid-syntax': syntax,
               'data-invalid-message': 'Footnotes must be set a content at the first use of the each alias',
             },
         // FIXME: #36031
@@ -58,45 +70,20 @@ function build(group: string, marker: (index: number) => string): (target: Docum
           ? undefined
           : [html('a', { href: `#${defId}`, rel: 'noopener' }, marker(defIndex))])
         .firstChild as HTMLAnchorElement;
+      assert(ref.title || ref.matches('.invalid'));
       assert(ref.firstChild);
-      if (def) {
-        void def.lastChild!.appendChild(
-          html('a', { href: `#${refId}`, rel: 'noopener' }, ` ~${refIndex}`));
-        if (title && def.childNodes.length === 1) {
-          const content = contents.get(ref)!;
-          assert(content.length > 0);
-          void def.insertBefore(
-            content[0].parentNode === ref
-              ? frag(content)
-              : frag(content).cloneNode(true),
-            def.lastChild);
-          assert(def.childNodes.length > 1);
-        }
-        continue;
-      }
-      else {
-        const content = contents.get(ref)!;
-        void defs.set(identifier,
-          html('li', { id: defId, class: 'footnote' }, [
-            content.length === 0 || content[0].parentNode === ref
-              ? frag(content)
-              : frag(content).cloneNode(true),
-            html('sup', [
-              html('a', { href: `#${refId}`, rel: 'noopener' }, ` ~${refIndex}`),
-            ]),
-          ]));
-        continue;
-      }
+      void def.lastChild!.appendChild(
+        html('a', { href: `#${refId}`, rel: 'noopener' }, ` ~${refIndex}`));
     }
     count = 0;
     I:
     for (const def of defs.values()) {
       void ++count;
       while (footnote.children.length > defs.size) {
-        if (footnote.children[count - 1].outerHTML === def.outerHTML) continue I;
+        if (compare(footnote.children[count - 1], def)) continue I;
         yield footnote.removeChild(footnote.children[count - 1]) as HTMLLIElement;
       }
-      if (footnote.children.length >= count && footnote.children[count - 1].outerHTML === def.outerHTML) continue;
+      if (footnote.children.length >= count && compare(footnote.children[count - 1], def)) continue;
       yield footnote.insertBefore(def, footnote.children[count - 1] || null);
     }
     while (footnote.children.length > defs.size) {
@@ -104,4 +91,9 @@ function build(group: string, marker: (index: number) => string): (target: Docum
     }
     return;
   }
+}
+
+function compare(a: Element, b: HTMLElement): boolean {
+  return a.id === b.id
+      && a.innerHTML === b.innerHTML;
 }
