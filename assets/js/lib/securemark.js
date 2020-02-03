e2e7a1894e88681286be08ed5f34912a13d42713
Travis-CI <Travis-CI>
2020-02-03 01:59:26 +0000

    Update web contents

--- assets/js/lib/securemark.js
+++ assets/js/lib/securemark.js
@@ -1,4 +1,4 @@
-/*! securemark v0.145.0 https://github.com/falsandtru/securemark | (c) 2017, falsandtru | UNLICENSED */
+/*! securemark v0.146.0 https://github.com/falsandtru/securemark | (c) 2017, falsandtru | UNLICENSED */
 require = function () {
     function r(e, n, t) {
         function o(i, f) {
