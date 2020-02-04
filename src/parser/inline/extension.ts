30eb2c587ef6fc26bb3516d0261293abbc73ca5d
falsandtru <falsandtru@users.noreply.github.com>
2020-02-04 09:08:18 +0900

    Optimization

--- src/parser/inline/extension.ts
+++ src/parser/inline/extension.ts
@@ -5,7 +5,7 @@ import { label } from './extension/label';
 import { data } from './extension/data';
 import { placeholder } from './extension/placeholder';
 
-export const extension: ExtensionParser = guard(config => config.syntax?.inline?.extension ?? true, validate(/^[[$]/, union([
+export const extension: ExtensionParser = guard(config => config.syntax?.inline?.extension ?? true, validate(['[', '$'], union([
   index,
   label,
   data,
