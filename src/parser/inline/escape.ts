6bf8a688d7c464bae0ee73acaacb46b7fa2cc824
falsandtru <falsandtru@users.noreply.github.com>
2020-02-15 12:07:12 +0900

    Refactoring

--- src/parser/inline/escape.ts
+++ src/parser/inline/escape.ts
@@ -10,17 +10,17 @@ export const escape: EscapeParser = creator(union([(source, context) => {
     case '*':
       if (source.length < 4) return;
       assert(source[3]);
-      return source[1] === source[0]
+      return source[3] === source[0]
           && source[2] === source[0]
-          && source[3] === source[0]
+          && source[1] === source[0]
         ? repeat(source, context)
         : void 0;
     case '+':
     case '~':
     case '=':
       assert(source[2]);
-      return source[1] === source[0]
-          && source[2] === source[0]
+      return source[2] === source[0]
+          && source[1] === source[0]
         ? repeat(source, context)
         : void 0;
     default:
