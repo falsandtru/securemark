a3e77677d44ae358907b7bc78e40ba03e1a60930
falsandtru <falsandtru@users.noreply.github.com>
2020-02-15 15:38:35 +0900

    Refactoring

--- src/parser/segment.ts
+++ src/parser/segment.ts
@@ -19,7 +19,8 @@ const parser: SegmentParser = union([
 export function segment(source: string): string[] {
   assert(source === normalize(source));
   if (source.length > 1000 * 1000) return ['# ***Too large input over 1,000,000 characters***'];
-  assert(source.length < 2 ** 32 / 2);
+  // Don't use ** operator because it disables derequire.
+  assert(source.length < Math.pow(2, 32) / 2);
   const segments: string[] = [];
   while (source !== '') {
     const rest = exec(parser(source, {}));
