851cec07b358a6b4b99dea6fd0ec68656d165ee7
falsandtru <falsandtru@users.noreply.github.com>
2020-02-22 18:31:43 +0900

    Refactoring

--- src/parser/inline/htmlentity.ts
+++ src/parser/inline/htmlentity.ts
@@ -5,6 +5,6 @@ import { html } from 'typed-dom';
 const parser = html('span');
 
 export const htmlentity: HTMLEntityParser = creator(focus(
-  /^&[0-9a-z]+;/i,
+  /^&[0-9A-Za-z]+;/,
   entity =>
     [[[parser.innerHTML = entity as never, parser.firstChild!.cloneNode() as Text][1]], '']));
