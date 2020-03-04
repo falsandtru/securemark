25c8051f8d32b31effe0003fb56732168ddb1eda
falsandtru <falsandtru@users.noreply.github.com>
2020-03-04 14:41:27 +0900

    Refactoring

--- src/parser/inline/ruby.ts
+++ src/parser/inline/ruby.ts
@@ -44,11 +44,13 @@ export const ruby: RubyParser = lazy(() => creator(bind(
   })));
 
 const text: RubyParser.TextParser = creator((source, context) => {
+  const { resource } = context;
   const next = /[\s\\&]/;
   const acc = [''];
   let printable = false;
   while (source !== '') {
     assert(source[0] !== '\n');
+    resource && void --resource.creation;
     const i = source.search(next);
     switch (i) {
       case -1:
