144269ec09d045abf62fe65773fd43c5d5fdae95
falsandtru <falsandtru@users.noreply.github.com>
2020-03-13 03:16:49 +0900

    Refactoring

--- src/parser/inline/autolink/url.ts
+++ src/parser/inline/autolink/url.ts
@@ -1,5 +1,5 @@
 import { AutolinkParser } from '../../inline';
-import { Parser, union, some, validate, rewrite, context, creator, convert, surround, open, lazy } from '../../../combinator';
+import { union, some, validate, rewrite, context, creator, convert, surround, open, lazy } from '../../../combinator';
 import { unescsource, str } from '../../source';
 import { link } from '../link';
 
@@ -8,7 +8,7 @@ const closer = /^[-+*~^,.;:!?]*(?=[\s"`|\[\](){}<>]|\\?(?:$|\s))/;
 export const url: AutolinkParser.UrlParser = lazy(() => rewrite(
   validate(['http', 'ttp'], open(
     str(/^h?ttps?:\/\/(?=[^/?#\s])/),
-    some<Parser<string>>(union([bracket, some(unescsource, closer)])))),
+    some(union([bracket, some(unescsource, closer)])))),
   convert(
     source => `{ ${address(source)}${attribute(source)} }`,
     context({ syntax: { inline: { link: void 0 } } },
