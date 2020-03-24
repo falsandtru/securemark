613e95113f2fe9149db9997b60a2fc9cc4f38f2f
falsandtru <falsandtru@users.noreply.github.com>
2020-03-24 17:59:12 +0900

    Refactoring

--- src/parser/block/paragraph/mention/quotation.ts
+++ src/parser/block/paragraph/mention/quotation.ts
@@ -1,11 +1,11 @@
 import { ParagraphParser } from '../../../block';
-import { union, some, block, validate, rewrite, creator, fmap, convert, lazy } from '../../../../combinator';
+import { union, some, block, validate, rewrite, creator, fmap, convert } from '../../../../combinator';
 import { defrag } from '../../../util';
 import { contentline } from '../../../source';
 import { autolink } from '../../../autolink';
 import { html } from 'typed-dom';
 
-export const quotation: ParagraphParser.MentionParser.QuotationParser = lazy(() => block(creator(fmap(
+export const quotation: ParagraphParser.MentionParser.QuotationParser = block(creator(fmap(
   union([
     rewrite(
       some(validate(/^>+(?:$|\s)/, contentline)),
@@ -15,4 +15,4 @@ export const quotation: ParagraphParser.MentionParser.QuotationParser = lazy(() 
       convert(source => source.replace(/\n$/, ''), some(autolink))),
   ]),
   ns => [html('span', { class: 'quotation' }, defrag(ns))])),
-  false));
+  false);
