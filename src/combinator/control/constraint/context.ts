9cc7c0b3ef3398fff773663bb51b73a4194837ae
falsandtru <falsandtru@users.noreply.github.com>
2020-03-25 05:25:05 +0900

    Refactoring

--- src/combinator/control/constraint/context.ts
+++ src/combinator/control/constraint/context.ts
@@ -1,8 +1,8 @@
 import { WeakMap } from 'spica/global';
-import { hasOwnProperty, ObjectCreate } from 'spica/alias';
+import { ObjectCreate } from 'spica/alias';
 import { Parser, Ctx, Context } from '../../data/parser';
 import { template } from 'spica/assign';
-import { type } from 'spica/type';
+import { type, isPrimitive } from 'spica/type';
 import { memoize } from 'spica/memoize';
 
 export function guard<P extends Parser<unknown>>(f: (context: Context<P>) => boolean, parser: P): P;
@@ -16,42 +16,44 @@ export function guard<T extends unknown, D extends Parser<unknown, any>[]>(f: (c
 export function update<P extends Parser<unknown>>(context: Context<P>, parser: P): P;
 export function update<T extends unknown, D extends Parser<unknown, any, C>[], C extends Ctx>(base: C, parser: Parser<T, D, C>): Parser<T, D, C> {
   assert(Object.getPrototypeOf(base) === Object.prototype);
+  assert(Object.freeze(base));
   return (source, context) =>
-    parser(source, merge(ObjectCreate(context), base));
+    parser(source, inherit(ObjectCreate(context), base));
 }
 
 export function context<P extends Parser<unknown>>(context: Context<P>, parser: P): P;
 export function context<T extends unknown, D extends Parser<unknown, any, C>[], C extends Ctx>(base: C, parser: Parser<T, D, C>): Parser<T, D, C> {
   assert(Object.getPrototypeOf(base) === Object.prototype);
-  const merge_ = memoize<C, C>(context => merge(ObjectCreate(context), base), new WeakMap());
+  assert(Object.freeze(base));
+  const inherit_ = memoize<C, C>(context => inherit(ObjectCreate(context), base), new WeakMap());
   return (source, context) =>
-    parser(source, merge_(context));
+    parser(source, inherit_(context));
 }
 
-const merge = template((prop, target, source) => {
+const inherit = template((prop, target, source) => {
+  assert(!Object.isSealed(target));
+  assert(Object.isExtensible(target));
+  assert(!target.hasOwnProperty(prop));
+  //assert(Object.freeze(source));
   switch (prop) {
     case 'resources':
       assert(typeof source[prop] === 'object');
-      return prop in target
-        ? target[prop]
-        : target[prop] = ObjectCreate(source[prop]);
+      if (prop in target) return;
+      return target[prop] = ObjectCreate(source[prop]);
   }
   switch (type(source[prop])) {
     case 'Object':
       assert(Object.getPrototypeOf(source[prop]) === Object.prototype);
+      //assert(Object.freeze(source[prop]));
       switch (type(target[prop])) {
         case 'Object':
-          return target[prop] = isOwnProperty(target, prop)
-            ? merge(target[prop], source[prop])
-            : merge(ObjectCreate(target[prop]), source[prop]);
+          return target[prop] = inherit(ObjectCreate(target[prop]), source[prop]);
         default:
           return target[prop] = ObjectCreate(source[prop]);
       }
     default:
-      return target[prop] = source[prop];
+      return target[prop] = isPrimitive(source[prop])
+        ? source[prop]
+        : ObjectCreate(source[prop]);
   }
 });
-
-const isOwnProperty: (o: object, p: string) => boolean = '__proto__' in {}
-  ? (o, p) => !('__proto__' in o) || o[p] !== o['__proto__'][p]
-  : hasOwnProperty;
