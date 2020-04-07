30c3d86260181bff727494eaf3a8b2440872012a
falsandtru <falsandtru@users.noreply.github.com>
2020-04-08 04:54:31 +0900

    Refactoring

--- src/parser/inline/comment.ts
+++ src/parser/inline/comment.ts
@@ -4,7 +4,7 @@ import { html } from 'typed-dom';
 
 export const comment: CommentParser = creator(validate('<#', match(
   /^<(#+)\s+(\S+(?:\s+(?!\1)\S+)*)(\s+\1>)?/,
-  ([whole, , title, last]) => (rest, { resources }) =>
-    last
+  ([whole, , title, closer]) => (rest, { resources }) =>
+    closer
       ? [[html('sup', { class: 'comment', title })], rest]
       : resources && void (resources.creation -= whole.match(/<#+\s/g)!.length))));
