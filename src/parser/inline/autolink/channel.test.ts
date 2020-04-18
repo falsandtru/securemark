46f696de36e2b80f5e148dedd9b7bbe59da734fd
falsandtru <falsandtru@users.noreply.github.com>
2020-04-19 06:45:32 +0900

    Refactoring

--- src/parser/inline/autolink/channel.test.ts
+++ src/parser/inline/autolink/channel.test.ts
@@ -9,11 +9,11 @@ describe('Unit: parser/inline/autolink/channel', () => {
     it('invalid', () => {
       assert.deepStrictEqual(inspect(parser('')), undefined);
       assert.deepStrictEqual(inspect(parser('@a@')), [['@a@'], '']);
-      assert.deepStrictEqual(inspect(parser('@a@1')), [['@a@1'], '']);
+      assert.deepStrictEqual(inspect(parser('@a@b')), [['@a@b'], '']);
       assert.deepStrictEqual(inspect(parser('@a#')), [['@a#'], '']);
       assert.deepStrictEqual(inspect(parser('@a#1')), [['@a#1'], '']);
       assert.deepStrictEqual(inspect(parser('@a#b@')), [['@a#b@'], '']);
-      assert.deepStrictEqual(inspect(parser('@a#b@1')), [['@a#b@1'], '']);
+      assert.deepStrictEqual(inspect(parser('@a#1@b')), [['@a#1@b'], '']);
       assert.deepStrictEqual(inspect(parser('@a#b#')), [['@a#b#'], '']);
       assert.deepStrictEqual(inspect(parser('@a#b#1')), [['@a#b#1'], '']);
       assert.deepStrictEqual(inspect(parser(' @a#b')), undefined);
