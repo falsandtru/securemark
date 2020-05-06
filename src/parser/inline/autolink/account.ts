6f70611abd341d9431e07c63f5c6cd4cd01b053c
falsandtru <falsandtru@users.noreply.github.com>
2020-05-07 01:31:44 +0900

    Refactoring

--- src/parser/inline/autolink/account.ts
+++ src/parser/inline/autolink/account.ts
@@ -5,7 +5,7 @@ import { html } from 'typed-dom';
 // https://example.com/@user must be a user page or a redirect page going there.
 
 export const account: AutolinkParser.AccountParser = creator(validate('@', focus(
-  /^@(?:(?![^/]*?--)[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9]{1,63}){1,2}\/)?[A-Z-a-z][A-Za-z0-9]*(?:-[A-Za-z0-9]+)*/,
+  /^@(?:(?![a-z0-9.-]{0,200}?--)[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9]{1,63}){1,2}\/)?[A-Z-a-z][A-Za-z0-9]*(?:-[A-Za-z0-9]+)*/,
   source => [[html('a', { class: 'account', rel: 'noopener', 'data-ns': namespace(source) }, source)], ''])));
 
 function namespace(source: string): string | undefined {
