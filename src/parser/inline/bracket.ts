29e2f33656024992f6d6965c12d8f7efbb938793
falsandtru <falsandtru@users.noreply.github.com>
2020-06-10 09:54:24 +0900

    Refactoring

--- src/parser/inline/bracket.ts
+++ src/parser/inline/bracket.ts
@@ -1,6 +1,6 @@
 import { undefined } from 'spica/global';
 import { BracketParser, inline } from '../inline';
-import { union, some, surround, lazy } from '../../combinator';
+import { union, some, context, surround, lazy } from '../../combinator';
 import { char } from '../source';
 import { unshift } from 'spica/array';
 
@@ -8,5 +8,5 @@ export const bracket: BracketParser = lazy(() => union([
   surround(char('('), some(inline, ')'), char(')'), true, undefined, ([as, bs = []], rest) => [unshift(as, bs), rest]),
   surround(char('['), some(inline, ']'), char(']'), true, undefined, ([as, bs = []], rest) => [unshift(as, bs), rest]),
   surround(char('{'), some(inline, '}'), char('}'), true, undefined, ([as, bs = []], rest) => [unshift(as, bs), rest]),
-  surround(char('"'), some(inline, '"', '"'), char('"'), true, undefined, ([as, bs = []], rest) => [unshift(as, bs), rest]),
+  surround(char('"'), context({ syntax: { inline: { media: false } } }, some(inline, '"', '"')), char('"'), true, undefined, ([as, bs = []], rest) => [unshift(as, bs), rest]),
 ]));
