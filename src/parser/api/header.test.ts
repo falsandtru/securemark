ffad1ffd00a24d29c2b2ab0f99d7200ef80d2012
falsandtru <falsandtru@users.noreply.github.com>
2020-06-28 02:46:48 +0900

    Refactoring

--- src/parser/api/header.test.ts
+++ src/parser/api/header.test.ts
@@ -2,13 +2,18 @@ import { header } from './header';
 
 describe('Unit: parser/api/header', () => {
   describe('header', () => {
-    it('', () => {
+    it('invalid', () => {
       assert.deepStrictEqual(header(''), undefined);
       assert.deepStrictEqual(header('---'), undefined);
       assert.deepStrictEqual(header('---\n'), undefined);
+      assert.deepStrictEqual(header('---\na: b\n'), undefined);
+      assert.deepStrictEqual(header('---\n \n---'), undefined);
+      assert.deepStrictEqual(header('---\n-\n---'), undefined);
+    });
+
+    it('basic', () => {
       assert.deepStrictEqual(header('---\n---'), []);
       assert.deepStrictEqual(header('---\n---\n'), []);
-      assert.deepStrictEqual(header('---\na: b\n'), undefined);
       assert.deepStrictEqual(header('---\na: b\n---'), ['a: b']);
       assert.deepStrictEqual(header('---\na: b\nc: d\n---'), ['a: b', 'c: d']);
       assert.deepStrictEqual(header('---\na: b\nc: d\n---\n'), ['a: b', 'c: d']);
--- src/parser/header.ts
+++ src/parser/header.ts
@@ -6,12 +6,12 @@ export const header: MarkdownParser.HeaderParser = block(validate('---', fmap(
   fence(/^(---)[^\S\n]*(?:$|\n)/, 100, true),
   // Bug: Type mismatch between outer and inner.
   ([body, closer, opener, delim]: string[]) =>
-    closer
+    closer && body.search(/^(?![A-Za-z]|$)/m) === -1
       // TODO: Set the specified base URL.
-      ? [html('div', { class: 'header' }, body.replace(/\n$/, ''))]
+      ? [html('div', { class: 'header' }, body.slice(0, -1))]
       : [html('pre', {
           class: `header notranslate invalid`,
           'data-invalid-syntax': 'header',
           'data-invalid-type': 'closer',
-          'data-invalid-message': `Missing closing delimiter ${delim}`,
+          'data-invalid-message': closer ? `All lines must start with alphabet characters` : `Missing closing delimiter ${delim}`,
         }, `${opener}${body}${closer}`)])));
