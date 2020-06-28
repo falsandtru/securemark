da1bd523c794fba146eb2c09ed92c1dead473102
falsandtru <falsandtru@users.noreply.github.com>
2020-06-28 22:19:41 +0900

    Refactoring

--- src/parser/api/body.ts
+++ src/parser/api/body.ts
@@ -4,6 +4,7 @@ import { header } from '../header';
 
 export function body(source: string): string {
   const rest = exec(header(source, {}));
+  assert(source.endsWith(rest || ''));
   return rest !== undefined
     ? rest.replace(/^[^\S\n]*\n?/, '')
     : source;
