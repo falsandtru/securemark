a56e5554129383d094102aad46eed66e79925d0b
falsandtru <falsandtru@users.noreply.github.com>
2020-06-30 16:00:35 +0900

    Refactoring

--- src/parser/normalize.ts
+++ src/parser/normalize.ts
@@ -5,9 +5,9 @@ export function normalize(source: string): string {
   const header = source.slice(0, source.length - rest.length);
   return header.replace(/(^|\S)[^\S\n]+(?=$|\n)$/mg, '$1') + rest
     .replace(/\u0000|[\uD800-\uDBFF][\uDC00-\uDFFF]?|[\uDC00-\uDFFF]/g, str =>
-      str.length === 2
-        ? str
-        : '\uFFFD')
+      str.length === 1
+        ? '\uFFFD'
+        : str)
     .replace(/\r\n|[\x00-\x08\x0B-\x1F\x7F]/g, char => {
       assert(!char.match(/^[\n\t]$/));
       switch (char) {
