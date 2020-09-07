52d2e4c0bcaa93d9df332c8d8d5e8234a7610d64
falsandtru <falsandtru@users.noreply.github.com>
2020-09-08 01:01:21 +0900

    Refactoring

--- src/parser/block/paragraph/mention/address.ts
+++ src/parser/block/paragraph/mention/address.ts
@@ -5,7 +5,7 @@ import { str } from '../../../source/str';
 import { html } from 'typed-dom';
 
 export const address: ParagraphParser.MentionParser.AddressParser = validate(
-  /^(>+)(?!>)[0-9][A-Za-z0-9]*(?:-[A-Za-z0-9]+)*[^\S\n]*(?:$|\n(?:\1[0-9][A-Za-z0-9]*(?:-[A-Za-z0-9]+)*[^\S\n]*(?:$|\n))*(?!\1))/,
+  /^(>+)[0-9][A-Za-z0-9]*(?:-[A-Za-z0-9]+)*[^\S\n]*(?:$|\n(?:\1[0-9][A-Za-z0-9]*(?:-[A-Za-z0-9]+)*[^\S\n]*(?:$|\n))*(?!\1))/,
   some(line(fmap(
     tails([
       str(/^>*(?=>)/),
