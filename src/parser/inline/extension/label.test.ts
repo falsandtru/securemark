e63d3b0922a671b57acb4b27bb50bb6e740c44ee
falsandtru <falsandtru@users.noreply.github.com>
2020-09-11 11:24:03 +0900

    Refactoring

--- src/parser/inline/extension/label.test.ts
+++ src/parser/inline/extension/label.test.ts
@@ -8,38 +8,30 @@ describe('Unit: parser/inline/extension/label', () => {
 
     it('invalid', () => {
       assert.deepStrictEqual(inspect(parser('[]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a-]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a--]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a-0')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a-0.]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a-.0]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a-00]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a-01]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a-0b]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a-b')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a-b-]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a-b-1]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a-b-0.]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a-b-.0]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a$-b]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$$a-b]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$$$-b]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a-0.0.0.0]')), undefined);
-      assert.deepStrictEqual(inspect(parser('[$a-1.1.1.1]')), undefined);
+      assert.deepStrictEqual(inspect(parser('$')), undefined);
+      assert.deepStrictEqual(inspect(parser('$a')), undefined);
+      assert.deepStrictEqual(inspect(parser('$a-')), undefined);
+      assert.deepStrictEqual(inspect(parser('$a--')), undefined);
+      assert.deepStrictEqual(inspect(parser('$a-.0')), undefined);
+      assert.deepStrictEqual(inspect(parser('$a-00')), undefined);
+      assert.deepStrictEqual(inspect(parser('$a-01')), undefined);
+      assert.deepStrictEqual(inspect(parser('$a-0b')), undefined);
+      assert.deepStrictEqual(inspect(parser('$a-b-1')), undefined);
+      assert.deepStrictEqual(inspect(parser('$$a-b')), undefined);
+      assert.deepStrictEqual(inspect(parser('$$$-b')), undefined);
+      assert.deepStrictEqual(inspect(parser('$a-0.0.0.0')), undefined);
+      assert.deepStrictEqual(inspect(parser('$a-1.1.1.1')), undefined);
     });
 
     it('basic', () => {
-      assert.deepStrictEqual(inspect(parser('[$a-b]')), [['<a class="label" data-label="a-b">$a-b</a>'], '']);
-      assert.deepStrictEqual(inspect(parser('[$a-b-0]')), [['<a class="label" data-label="a-b-0">$a-b-0</a>'], '']);
-      assert.deepStrictEqual(inspect(parser('[$a-b-0.0]')), [['<a class="label" data-label="a-b-0.0">$a-b-0.0</a>'], '']);
-      assert.deepStrictEqual(inspect(parser('[$a-b-0.0.0]')), [['<a class="label" data-label="a-b-0.0.0">$a-b-0.0.0</a>'], '']);
-      assert.deepStrictEqual(inspect(parser('[$a-0]')), [['<a class="label" data-label="a-0">$a-0</a>'], '']);
-      assert.deepStrictEqual(inspect(parser('[$-b]')), [['<a class="label" data-label="$-b">$-b</a>'], '']);
-      assert.deepStrictEqual(inspect(parser('[$-0]')), [['<a class="label" data-label="$-0">$-0</a>'], '']);
       assert.deepStrictEqual(inspect(parser('$a-b')), [['<a class="label" data-label="a-b">$a-b</a>'], '']);
+      assert.deepStrictEqual(inspect(parser('$a-b-0')), [['<a class="label" data-label="a-b-0">$a-b-0</a>'], '']);
+      assert.deepStrictEqual(inspect(parser('$a-b-0.0')), [['<a class="label" data-label="a-b-0.0">$a-b-0.0</a>'], '']);
+      assert.deepStrictEqual(inspect(parser('$a-b-0.0.0')), [['<a class="label" data-label="a-b-0.0.0">$a-b-0.0.0</a>'], '']);
+      assert.deepStrictEqual(inspect(parser('$a-0')), [['<a class="label" data-label="a-0">$a-0</a>'], '']);
       assert.deepStrictEqual(inspect(parser('$-b')), [['<a class="label" data-label="$-b">$-b</a>'], '']);
+      assert.deepStrictEqual(inspect(parser('$-0')), [['<a class="label" data-label="$-0">$-0</a>'], '']);
+      assert.deepStrictEqual(inspect(parser('[$a-b]')), [['<a class="label" data-label="a-b">$a-b</a>'], '']);
     });
 
   });
--- src/parser/inline/extension/label.ts
+++ src/parser/inline/extension/label.ts
@@ -4,7 +4,7 @@ import { str } from '../../source';
 import { html } from 'typed-dom';
 import { join } from 'spica/array';
 
-const body = str(/^(?:\$[a-z]*)(?:(?:-[a-z][0-9a-z]*)+(?:-0(?:\.0){0,2}(?!\.[0-9]))?|-(?:0|[1-9][0-9]*)(?:\.(?:0|[1-9][0-9]*)){0,2}(?!\.[0-9]))/);
+const body = str(/^(?:\$[a-z]*)(?:(?:-[a-z][0-9a-z]*)+(?:-0(?:\.0){0,2}(?!\.?[A-Za-z0-9]))?(?!-?[A-Za-z0-9])|-(?:0|[1-9][0-9]*)(?:\.(?:0|[1-9][0-9]*)){0,2}(?![.-]?[A-Za-z0-9]))/);
 
 export const segment: ExtensionParser.LabelParser.SegmentParser = clear(validate(['[$', '$'], union([
   surround('[', body, ']'),
