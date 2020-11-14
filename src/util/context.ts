84c8e3edf1415ce1404d857b5a775148aa7d309e
falsandtru <falsandtru@users.noreply.github.com>
2020-11-14 19:21:38 +0900

    Refactoring

--- src/util/context.ts
+++ src/util/context.ts
@@ -1,3 +1,5 @@
+import { undefined } from 'spica/global';
+
 export function context(
   base: DocumentFragment | HTMLElement | ShadowRoot,
   bound: string = 'id' in base && base.id
@@ -8,10 +10,15 @@ export function context(
   const context = 'id' in base && base.closest(bound) || null;
   return el => {
     assert(el.parentNode?.parentNode);
-    const node = memory.has(el.parentNode!)
-      ? el.parentNode!
-      : el.parentNode!.parentNode!;
-    return memory.get(node)
-        || memory.set(node, el.closest(bound) === context).get(node)!;
+    const { parentNode } = el;
+    const node = memory.has(parentNode!)
+      ? parentNode!
+      : parentNode!.parentNode!;
+    let result = memory.get(node);
+    if (result === undefined) {
+      result = el.closest(bound) === context;
+      memory.set(node, result);
+    }
+    return result;
   };
 }
