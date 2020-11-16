0581c4cca7504649570c45457a3c9e334a399463
falsandtru <falsandtru@users.noreply.github.com>
2020-11-16 19:22:04 +0900

    Refactoring

--- src/combinator/control/constraint/resource.ts
+++ src/combinator/control/constraint/resource.ts
@@ -7,9 +7,11 @@ export function creator(cost: number | Parser<unknown>, parser?: Parser<unknown>
   assert(cost > 0);
   return (source, context) => {
     const { resources } = context;
-    if (resources && resources.creation < 0) throw new Error('Too many creations');
+    if (!resources) return parser!(source, context);
+    assert(resources.creation !== undefined);
+    if (resources.creation < 0) throw new Error('Too many creations');
     const result = parser!(source, context);
-    if (result && resources) {
+    if (result) {
       resources.creation -= cost;
       assert(Object.getPrototypeOf(resources).creation % 10 === 0);
     }
