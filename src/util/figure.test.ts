51cbcf3e611337f25300db1a4713c465a7f96b6c
falsandtru <falsandtru@users.noreply.github.com>
2020-11-18 11:53:51 +0900

    Refactoring

--- src/util/figure.test.ts
+++ src/util/figure.test.ts
@@ -54,6 +54,22 @@ describe('Unit: util/figure', () => {
       }
     });
 
+    it('number', () => {
+      const target = parse([
+        '$-a\n$$\n$$',
+        '$-a',
+      ].join('\n\n'));
+      for (let i = 0; i < 2; ++i) {
+        [...figure(target)];
+        assert.deepStrictEqual(
+          [...target.children].map(el => el.outerHTML),
+          [
+            '<figure data-label="$-a" data-group="$" data-number="1" id="label:$-a"><div class="figcontent"><div class="math notranslate">$$\n$$</div></div><span class="figindex">(1)</span><figcaption></figcaption></figure>',
+            '<p><a class="label" data-label="$-a" href="#label:$-a">(1)</a></p>',
+          ]);
+      }
+    });
+
     it('fixed', () => {
       const target = parse([
         '$fig-2\n> ',
@@ -78,6 +94,26 @@ describe('Unit: util/figure', () => {
             '<p><a class="label" data-label="fig-2" href="#label:fig-2">Fig 2</a></p>',
             '<p><a class="label" data-label="$-4.1.1" href="#label:$-4.1.1">(4.1.1)</a></p>',
             '<p><a class="label" data-label="fig-1">$fig-1</a></p>',
+          ]);
+      }
+    });
+
+    it('separation', () => {
+      const target = html('blockquote', parse([
+        '!>> ~~~figure $fig-a\n>> > \n>>\n~~~\n> ~~~figure $fig-a\n> > \n>\n~~~',
+        '~~~~example/markdown\n~~~figure $fig-a\n> \n\n~~~\n\n$fig-a\n~~~~',
+        '~~~figure $fig-b\n> \n\n~~~',
+        '~~~figure $fig-a\n> \n\n~~~',
+      ].join('\n\n')).children);
+      for (let i = 0; i < 2; ++i) {
+        [...figure(target)];
+        assert.deepStrictEqual(
+          [...target.children].map(el => el.outerHTML),
+          [
+            '<blockquote><blockquote><figure data-label="fig-a" data-group="fig" data-number="1"><div class="figcontent"><blockquote></blockquote></div><span class="figindex">Fig 1. </span><figcaption></figcaption></figure></blockquote><figure data-label="fig-a" data-group="fig" data-number="1"><div class="figcontent"><blockquote></blockquote></div><span class="figindex">Fig 1. </span><figcaption></figcaption></figure></blockquote>',
+            '<aside class="example" data-type="markdown"><pre>~~~figure $fig-a\n&gt; \n\n~~~\n\n$fig-a</pre><div><figure data-label="fig-a" data-group="fig" data-number="1"><div class="figcontent"><blockquote></blockquote></div><span class="figindex">Fig 1. </span><figcaption></figcaption></figure><p><a class="label" data-label="fig-a">Fig 1</a></p></div><ol></ol><ol></ol></aside>',
+            '<figure data-label="fig-b" data-group="fig" data-number="1" id="label:fig-b"><div class="figcontent"><blockquote></blockquote></div><span class="figindex">Fig 1. </span><figcaption></figcaption></figure>',
+            '<figure data-label="fig-a" data-group="fig" data-number="2" id="label:fig-a"><div class="figcontent"><blockquote></blockquote></div><span class="figindex">Fig 2. </span><figcaption></figcaption></figure>',
           ]);
       }
     });
@@ -156,42 +192,6 @@ describe('Unit: util/figure', () => {
             '<figure data-label="fig-i" data-group="fig" data-number="5.4" id="label:fig-i"><div class="figcontent"><blockquote></blockquote></div><span class="figindex">Fig 5.4. </span><figcaption></figcaption></figure>',
             '<h1 id="index:0">0</h1>',
             '<figure data-label="fig-j" data-group="fig" data-number="6.1" id="label:fig-j"><div class="figcontent"><blockquote></blockquote></div><span class="figindex">Fig 6.1. </span><figcaption></figcaption></figure>',
-          ]);
-      }
-    });
-
-    it('number', () => {
-      const target = parse([
-        '$-a\n$$\n$$',
-        '$-a',
-      ].join('\n\n'));
-      for (let i = 0; i < 2; ++i) {
-        [...figure(target)];
-        assert.deepStrictEqual(
-          [...target.children].map(el => el.outerHTML),
-          [
-            '<figure data-label="$-a" data-group="$" data-number="1" id="label:$-a"><div class="figcontent"><div class="math notranslate">$$\n$$</div></div><span class="figindex">(1)</span><figcaption></figcaption></figure>',
-            '<p><a class="label" data-label="$-a" href="#label:$-a">(1)</a></p>',
-          ]);
-      }
-    });
-
-    it('separation', () => {
-      const target = html('blockquote', parse([
-        '!>> ~~~figure $fig-a\n>> > \n>>\n~~~\n> ~~~figure $fig-a\n> > \n>\n~~~',
-        '~~~~example/markdown\n~~~figure $fig-a\n> \n\n~~~\n\n$fig-a\n~~~~',
-        '~~~figure $fig-b\n> \n\n~~~',
-        '~~~figure $fig-a\n> \n\n~~~',
-      ].join('\n\n')).children);
-      for (let i = 0; i < 2; ++i) {
-        [...figure(target)];
-        assert.deepStrictEqual(
-          [...target.children].map(el => el.outerHTML),
-          [
-            '<blockquote><blockquote><figure data-label="fig-a" data-group="fig" data-number="1"><div class="figcontent"><blockquote></blockquote></div><span class="figindex">Fig 1. </span><figcaption></figcaption></figure></blockquote><figure data-label="fig-a" data-group="fig" data-number="1"><div class="figcontent"><blockquote></blockquote></div><span class="figindex">Fig 1. </span><figcaption></figcaption></figure></blockquote>',
-            '<aside class="example" data-type="markdown"><pre>~~~figure $fig-a\n&gt; \n\n~~~\n\n$fig-a</pre><div><figure data-label="fig-a" data-group="fig" data-number="1"><div class="figcontent"><blockquote></blockquote></div><span class="figindex">Fig 1. </span><figcaption></figcaption></figure><p><a class="label" data-label="fig-a">Fig 1</a></p></div><ol></ol><ol></ol></aside>',
-            '<figure data-label="fig-b" data-group="fig" data-number="1" id="label:fig-b"><div class="figcontent"><blockquote></blockquote></div><span class="figindex">Fig 1. </span><figcaption></figcaption></figure>',
-            '<figure data-label="fig-a" data-group="fig" data-number="2" id="label:fig-a"><div class="figcontent"><blockquote></blockquote></div><span class="figindex">Fig 2. </span><figcaption></figcaption></figure>',
           ]);
       }
     });
