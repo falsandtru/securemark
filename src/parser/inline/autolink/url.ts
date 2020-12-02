2dc2585b8adf32d23ed50a142b6272154695e78d
falsandtru <falsandtru@users.noreply.github.com>
2020-12-03 07:11:13 +0900

    Refactoring

--- src/parser/inline/autolink/url.ts
+++ src/parser/inline/autolink/url.ts
@@ -1,19 +1,19 @@
 import { undefined } from 'spica/global';
 import { AutolinkParser } from '../../inline';
 import { union, some, validate, focus, rewrite, context, creator, convert, surround, open, lazy } from '../../../combinator';
-import { unescsource, str } from '../../source';
 import { link } from '../link';
+import { unescsource } from '../../source';
 
 const closer = /^[-+*~^,.;:!?]*(?=[\s"`|\[\](){}<>]|\\?(?:$|\s))/;
 
-export const url: AutolinkParser.UrlParser = lazy(() => rewrite(
-  validate('http', open(
-    str(/^https?:\/\/(?=[\x00-\x7F])/),
-    focus(/^(?:(?!\s)[\x00-\x7F])+/, some(union([bracket, some(unescsource, closer)]))))),
+export const url: AutolinkParser.UrlParser = lazy(() => validate('http', rewrite(
+  open(
+    /^https?:\/\/(?=[\x00-\x7F])/,
+    focus(/^(?:(?!\s)[\x00-\x7F])+/, some(union([bracket, some(unescsource, closer)])))),
   convert(
     url => `{ ${url} }`,
     context({ syntax: { inline: { link: undefined } } },
-    union([link])))));
+    union([link]))))));
 
 const bracket: AutolinkParser.UrlParser.BracketParser = lazy(() => creator(union([
   surround('(', some(union([bracket, unescsource]), ')'), ')', true),
