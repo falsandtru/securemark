7bce87e8d238a2a1272c72605818a9b791128857
falsandtru <falsandtru@users.noreply.github.com>
2020-12-10 11:05:21 +0900

    Refactoring

--- src/parser/inline/math.ts
+++ src/parser/inline/math.ts
@@ -1,17 +1,13 @@
 import { undefined } from 'spica/global';
 import { MathParser } from '../inline';
-import { union, creator, bind, surround } from '../../combinator';
+import { union, creator, fmap, surround } from '../../combinator';
 import { str } from '../source';
 import { html } from 'typed-dom';
 
-export const math: MathParser = creator(bind(
-  surround('${', union([str(/^[^\n]+?(?=}\$)/)]), '}$'),
-  ([source], rest, { caches: { math: cache = undefined } = {} }) =>
-    (source = `\${${source.trim()}}$`) === '${}$'
-      ? undefined
-      : [
-          cache?.has(source)
-            ? [cache.get(source)!.cloneNode(true)]
-            : [html('span', { class: 'math notranslate', 'data-src': source }, source)],
-          rest
-        ]));
+export const math: MathParser = creator(fmap(
+  surround('${', union([str(/^[^\S\n]*(?!}\$)\S[^\n]*?(?=}\$)/)]), '}$'),
+  ([source], _, { caches: { math: cache = undefined } = {} }) => [
+    (source = `\${${source.trim()}}$`) && cache?.has(source)
+      ? cache.get(source)!.cloneNode(true)
+      : html('span', { class: 'math notranslate', 'data-src': source }, source)
+  ]));
