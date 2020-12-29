bedab95f536423a2310092fde69d19c244d988c3
falsandtru <falsandtru@users.noreply.github.com>
2020-12-30 03:00:08 +0900

    Refactoring

--- src/parser/inline/code.test.ts
+++ src/parser/inline/code.test.ts
@@ -9,6 +9,9 @@ describe('Unit: parser/inline/code', () => {
     it('invalid', () => {
       assert.deepStrictEqual(inspect(parser('')), undefined);
       assert.deepStrictEqual(inspect(parser('`')), undefined);
+      assert.deepStrictEqual(inspect(parser('``')), undefined);
+      assert.deepStrictEqual(inspect(parser('``a`')), undefined);
+      assert.deepStrictEqual(inspect(parser('`a``')), undefined);
       assert.deepStrictEqual(inspect(parser('`\n`')), undefined);
       assert.deepStrictEqual(inspect(parser('`a\nb`')), undefined);
       assert.deepStrictEqual(inspect(parser('`a\\\nb`')), undefined);
@@ -20,7 +23,6 @@ describe('Unit: parser/inline/code', () => {
       assert.deepStrictEqual(inspect(parser('`  `')), [['<code data-src="`  `">  </code>'], '']);
       assert.deepStrictEqual(inspect(parser('`a`')), [['<code data-src="`a`">a</code>'], '']);
       assert.deepStrictEqual(inspect(parser('`ab`')), [['<code data-src="`ab`">ab</code>'], '']);
-      assert.deepStrictEqual(inspect(parser('`a``')), [['`'], 'a``']);
       assert.deepStrictEqual(inspect(parser('`a`b')), [['<code data-src="`a`">a</code>'], 'b']);
       assert.deepStrictEqual(inspect(parser('`a`b`')), [['<code data-src="`a`">a</code>'], 'b`']);
     });
@@ -30,8 +32,6 @@ describe('Unit: parser/inline/code', () => {
       assert.deepStrictEqual(inspect(parser('`\\\\`')), [['<code data-src="`\\\\`">\\\\</code>'], '']);
       assert.deepStrictEqual(inspect(parser('`&nbsp;`')), [['<code data-src="`&amp;nbsp;`">&amp;nbsp;</code>'], '']);
       assert.deepStrictEqual(inspect(parser('` `` `')), [['<code data-src="` `` `">``</code>'], '']);
-      assert.deepStrictEqual(inspect(parser('``')), [['``'], '']);
-      assert.deepStrictEqual(inspect(parser('``a`')), [['``'], 'a`']);
       assert.deepStrictEqual(inspect(parser('`` ` ``')), [['<code data-src="`` ` ``">`</code>'], '']);
     });
 
--- src/parser/inline/code.ts
+++ src/parser/inline/code.ts
@@ -3,8 +3,6 @@ import { validate, creator, match } from '../../combinator';
 import { html } from 'typed-dom';
 
 export const code: CodeParser = creator(validate('`', '`', '\n', match(
-  /^(`+)(?!`)(?:([^\n]*?[^`\n])\1(?!`))?/,
+  /^(`+)(?!`)([^\n]*?[^`\n])\1(?!`)/,
   ([whole, , body]) => rest =>
-    body
-      ? [[html('code', { 'data-src': whole }, body.trim() || body)], rest]
-      : [[whole], rest])));
+    [[html('code', { 'data-src': whole }, body.trim() || body)], rest])));
