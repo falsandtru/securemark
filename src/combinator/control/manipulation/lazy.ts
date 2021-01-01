9253f1bf054d4d360a82065580f0c58aa6f546b9
falsandtru <falsandtru@users.noreply.github.com>
2021-01-01 21:58:17 +0900

    Refactoring

--- src/combinator/control/manipulation/lazy.ts
+++ src/combinator/control/manipulation/lazy.ts
@@ -3,6 +3,8 @@ import { Parser } from '../../data/parser';
 export function lazy<P extends Parser<unknown>>(builder: () => P): P;
 export function lazy<T, D extends Parser<unknown>[]>(builder: () => Parser<T, D>): Parser<T, D> {
   let parser: Parser<T, D> | undefined;
-  return (source, context) =>
-    (parser ? parser : parser = builder())(source, context);
+  return (source, context) => {
+    parser ? parser : parser = builder();
+    return parser(source, context);
+  };
 }
