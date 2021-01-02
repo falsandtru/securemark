ff3d4b97880bfd59d6b91fccab6fb49f2ff73b21
falsandtru <falsandtru@users.noreply.github.com>
2021-01-02 12:10:35 +0900

    Refactoring

--- src/parser/api/bind.ts
+++ src/parser/api/bind.ts
@@ -1,5 +1,5 @@
 import { undefined, location } from 'spica/global';
-import { ObjectAssign, ObjectCreate } from 'spica/alias';
+import { ObjectAssign } from 'spica/alias';
 import { ParserSettings, Progress } from '../../..';
 import { MarkdownParser } from '../../../markdown';
 import { eval } from '../../combinator';
@@ -21,12 +21,11 @@ export function bind(target: DocumentFragment | HTMLElement | ShadowRoot, settin
   nearest: (position: number) => HTMLElement | undefined;
   position: (block: HTMLElement) => number;
 } {
-  const { footnotes } = settings;
-  settings = ObjectAssign(ObjectCreate(settings), {
+  let context: MarkdownParser.Context = ObjectAssign({}, settings, {
     host: settings.host ?? new URL(location.pathname, location.origin),
     footnotes: undefined,
   });
-  if (settings.host?.origin === 'null') throw new Error(`Invalid host: ${settings.host.href}`);
+  if (context.host?.origin === 'null') throw new Error(`Invalid host: ${context.host.href}`);
   assert(!settings.id);
   type Block = readonly [string, readonly HTMLElement[], string];
   const blocks: Block[] = [];
@@ -43,9 +42,7 @@ export function bind(target: DocumentFragment | HTMLElement | ShadowRoot, settin
   function* parse(source: string): Generator<Progress, undefined, undefined> {
     const rev = revision = Symbol();
     const url = headers(source).find(field => field.toLowerCase().startsWith('url:'))?.slice(4).trim() || '';
-    const context: MarkdownParser.Context = ObjectAssign(settings, {
-      url: url ? new URL(url, url) : undefined,
-    });
+    context = ObjectAssign(context, { url: url ? new URL(url, url) : undefined });
     source = normalize(source);
     const sourceSegments: string[] = [];
     for (const seg of segment(source)) {
@@ -116,14 +113,14 @@ export function bind(target: DocumentFragment | HTMLElement | ShadowRoot, settin
       yield { type: 'block', value: el };
       if (rev !== revision) return yield { type: 'cancel' };
     }
-    for (const el of footnote(target, footnotes, context)) {
+    for (const el of footnote(target, settings.footnotes, context)) {
       assert(rev === revision);
       el
         ? yield { type: 'footnote', value: el }
         : yield { type: 'break' };
       if (rev !== revision) return yield { type: 'cancel' };
     }
-    for (const el of figure(target, footnotes, context)) {
+    for (const el of figure(target, settings.footnotes, context)) {
       assert(rev === revision);
       el
         ? yield { type: 'figure', value: el }
--- src/parser/api/parse.ts
+++ src/parser/api/parse.ts
@@ -12,7 +12,6 @@ import { figure } from '../../util/figure';
 import { footnote } from '../../util/footnote';
 import { memoize } from 'spica/memoize';
 import { URL } from 'spica/url';
-import { push } from 'spica/array';
 import { frag } from 'typed-dom';
 
 interface Options extends ParserOptions {
@@ -21,23 +20,28 @@ interface Options extends ParserOptions {
 }
 
 const inherit = memoize<MarkdownParser.Context, MarkdownParser.Context>(context => ObjectCreate(context), new WeakMap());
+const inherit2 = memoize<MarkdownParser.Context, (url: string) => MarkdownParser.Context>(context => memoize((_: string) => ObjectCreate(context)), new WeakMap());
 
 export function parse(source: string, opts: Options = {}, context?: MarkdownParser.Context): DocumentFragment {
   const url = headers(source).find(field => field.toLowerCase().startsWith('url:'))?.slice(4).trim() || '';
-  context = context ? inherit(context) : {};
-  context = ObjectAssign(context, opts, {
-    host: opts.host ?? context.host ?? new URL(location.pathname, location.origin),
-    url: url ? new URL(url, url) : context.url,
-    id: opts.id ?? context.id,
-    header: undefined,
-    test: undefined,
-    footnotes: undefined,
-  });
+  context = context && url === '' && context.id === opts.id
+    ? context
+    : ObjectAssign(context ? url ? inherit2(context)(url) : inherit(context) : {}, opts, {
+        host: opts.host ?? context?.host ?? new URL(location.pathname, location.origin),
+        url: url ? new URL(url, url) : context?.url,
+        id: opts.id ?? context?.id,
+        header: undefined,
+        test: undefined,
+        footnotes: undefined,
+      });
   if (context.host?.origin === 'null') throw new Error(`Invalid host: ${context.host.href}`);
-  const node = frag([...segment(normalize(source))]
-    .reduce((acc, seg, i) =>
-      push(acc, eval(i === 0 && opts.header !== false && header(seg, context!) || block(seg, context!), []))
-    , []));
+  const node = frag(function* () {
+    let head = opts.header ?? true;
+    for (const seg of segment(normalize(source))) {
+      yield* eval(head && header(seg, context) || block(seg, context), []);
+      head = false;
+    }
+  }());
   if (opts.test) return node;
   for (const _ of footnote(node, opts.footnotes, context));
   for (const _ of figure(node, opts.footnotes, context));
