849d05bb7ec2cbc1d0a23a92b1d5955620d8b603
falsandtru <falsandtru@users.noreply.github.com>
2021-06-15 03:34:41 +0900

    Refactoring

--- src/parser/api/header.ts
+++ src/parser/api/header.ts
@@ -2,23 +2,19 @@ import { eval, exec } from '../../combinator/data/parser';
 import { header as h } from '../header';
 
 export function header(source: string): string {
-  const result = h(source, {});
-  const [el] = eval(result, []);
-  return isValid(el)
-    ? source.slice(0, source.length - exec(result, source).length)
-    : '';
+  const [, rest = source] = parse(source);
+  return source.slice(0, source.length - rest.length);
 }
 
 export function headers(source: string): string[] {
-  const result = h(source, {});
-  const [el] = eval(result, []);
-  return isValid(el)
-    ? el.lastChild!.textContent!.split(/[^\S\n]*\n/)
-    : [];
+  const [el] = parse(source);
+  return el?.lastChild!.textContent!.split(/[^\S\n]*\n/) ?? [];
 }
 
-function isValid(el: HTMLElement | undefined): el is HTMLElement {
-  return !!el
-      && el.classList.contains('header')
-      && el.childNodes.length > 1;
+function parse(source: string): [HTMLDetailsElement, string] | [] {
+  const result = h(source, {});
+  const [el] = eval(result, []);
+  return el instanceof HTMLDetailsElement
+    ? [el, exec(result!)]
+    : [];
 }
