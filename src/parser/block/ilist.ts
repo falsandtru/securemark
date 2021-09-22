2b6de60d495321d438547ee271fc972821babdbf
falsandtru <falsandtru@users.noreply.github.com>
2021-09-22 19:50:49 +0900

    Refactoring

--- src/parser/block/ilist.ts
+++ src/parser/block/ilist.ts
@@ -1,5 +1,5 @@
 import { IListParser } from '../block';
-import { union, inits, some, block, line, validate, indent, creator, open, convert, trim, lazy, fmap } from '../../combinator';
+import { union, inits, some, block, line, validate, indent, context, creator, open, convert, trim, lazy, fmap } from '../../combinator';
 import { ulist_, fillFirstLine } from './ulist';
 import { olist_ } from './olist';
 import { inline } from '../inline';
@@ -7,6 +7,7 @@ import { html, defrag } from 'typed-dom';
 
 export const ilist: IListParser = lazy(() => block(fmap(validate(
   /^[-+*](?=[^\S\n]|\n[^\S\n]*\S)/,
+  context({ syntax: { inline: { media: false } } },
   some(creator(union([
     fmap(
       inits([
@@ -14,7 +15,7 @@ export const ilist: IListParser = lazy(() => block(fmap(validate(
         indent(union([ulist_, olist_, ilist_])),
       ]),
       ns => [html('li', defrag(fillFirstLine(ns)))]),
-  ])))),
+  ]))))),
   es => [html('ul', { class: 'invalid', 'data-invalid-syntax': 'list', 'data-invalid-type': 'syntax', 'data-invalid-description': 'Use "-" instead of "+" or "*".' }, es)])));
 
 export const ilist_: IListParser = convert(
