8ccc1b1c41a59042da15b228afdd0c7334577f9e
falsandtru <falsandtru@users.noreply.github.com>
2021-09-29 23:44:37 +0900

    Refactoring

--- src/parser/inline/comment.ts
+++ src/parser/inline/comment.ts
@@ -3,7 +3,7 @@ import { validate, creator, match } from '../../combinator';
 import { html } from 'typed-dom';
 
 export const comment: CommentParser = creator(validate('[#', '#]', match(
-  /^\[(#+)\s+((?:(?!\1\])\S+\s+)+)(\1\])?/,
+  /^\[(#+)\s+((?:\S+\s+)+?)(\1\]|$)/,
   ([, , title, closer]) => (rest, { resources }) =>
     closer
       ? [[html('sup', { class: 'comment', title: title.trim() })], rest]
