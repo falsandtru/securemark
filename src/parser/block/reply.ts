462f2cc72fd089ba42dac76f805002898bc8232d
falsandtru <falsandtru@users.noreply.github.com>
2022-03-09 02:59:03 +0900

    Refactoring

--- src/parser/block/reply.ts
+++ src/parser/block/reply.ts
@@ -10,18 +10,18 @@ import { html, defrag } from 'typed-dom';
 import { push, pop } from 'spica/array';
 
 export const reply: ReplyParser = block(validate('>', localize(fmap(
-  some(inits([
+  inits([
     some(inits([
       cite,
       quote,
     ])),
-    subsequence([
+    some(subsequence([
       some(quote),
       fmap(
         rewrite(
           some(anyline, delimiter),
           trim(visualize(some(inline)))),
         ns => push(ns, [html('br')])),
-    ]),
-  ])),
+    ])),
+  ]),
   ns => [html('p', defrag(pop(ns)[0]))]))));
