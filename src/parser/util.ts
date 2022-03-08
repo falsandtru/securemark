0b0eec2786f08824c62e69d2c395b90ba827c0a6
falsandtru <falsandtru@users.noreply.github.com>
2022-03-08 14:38:49 +0900

    Refactoring

--- src/parser/util.ts
+++ src/parser/util.ts
@@ -229,7 +229,7 @@ export function stringify(nodes: readonly (HTMLElement | string)[]): string {
     }
     else {
       assert(!node.matches('br') && !node.querySelector('br'));
-      // Note: Doesn't reflect line breaks.
+      // NOTE: Doesn't reflect line breaks.
       acc += node.innerText;
     }
   }
