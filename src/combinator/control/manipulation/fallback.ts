3c039cf7166a2f1791a85f9992806a3324c733b3
falsandtru <falsandtru@users.noreply.github.com>
2022-04-21 02:24:23 +0900

    Refactoring

--- src/combinator/control/manipulation/fallback.ts
+++ src/combinator/control/manipulation/fallback.ts
@@ -2,7 +2,6 @@ import { Parser, Tree, Context } from '../../data/parser';
 import { union } from '../../data/parser/union';
 
 export function fallback<P extends Parser<unknown>>(parser: P, otherwise: Parser<Tree<P>, Context<P>>): P;
-export function fallback<T, P extends Parser<T>>(parser: P, otherwise: Parser<T, Context<P>>): P;
 export function fallback<T>(parser: Parser<T>, otherwise: Parser<T>): Parser<T> {
   return union([parser, otherwise]);
 }
