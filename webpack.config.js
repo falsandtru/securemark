d83098b3436e0131e07773ed0f79a4d145f75908
falsandtru <falsandtru@users.noreply.github.com>
2022-05-20 21:02:28 +0900

    Refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = env => {
       path: path.resolve(__dirname, 'dist'),
       library: pkg.name,
       libraryTarget: 'umd',
-      globalObject: 'globalThis',
+      globalObject: 'this',
       clean: true,
     },
     module: {
