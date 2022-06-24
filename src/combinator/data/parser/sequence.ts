d9c5b58c29a244ecee5af5f7a4fed1e4acb27ccc
falsandtru <falsandtru@users.noreply.github.com>
2022-06-25 08:11:54 +0900

    Refactoring

--- src/combinator/data/parser/sequence.ts
+++ src/combinator/data/parser/sequence.ts
@@ -11,7 +11,7 @@ export function sequence<T, D extends Parser<T>[]>(parsers: D): Parser<T, Ctx, D
     let nodes: T[] | undefined;
     for (let i = 0, len = parsers.length; i < len; ++i) {
       if (rest === '') return;
-      if (context.delimiters?.match(rest, context.precedence)) break;
+      if (context.delimiters?.match(rest, context.precedence)) return;
       const result = parsers[i](rest, context);
       assert(check(rest, result));
       if (!result) return;
