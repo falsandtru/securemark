0a10c267e9eb92d6944ce046fefc75afccc1bdd3
falsandtru <falsandtru@users.noreply.github.com>
2022-06-25 22:31:26 +0900

    Refactoring

--- src/combinator/data/parser/context/memo.ts
+++ src/combinator/data/parser/context/memo.ts
@@ -5,13 +5,14 @@ export class Memo {
   public get length(): number {
     return this.memory.length;
   }
+  public offset = 0;
   public get(
     position: number,
     rule: number,
     syntax: number,
     state: number,
   ): readonly [any[], number] | undefined {
-    return this.memory[position - 1]?.[`${rule}:${syntax}:${state}`];
+    return this.memory[position + this.offset - 1]?.[`${rule}:${syntax}:${state}`];
   }
   public set(
     position: number,
@@ -21,10 +22,10 @@ export class Memo {
     nodes: any[],
     offset: number,
   ): void {
-    const record = this.memory[position - 1] ??= {};
+    const record = this.memory[position + this.offset - 1] ??= {};
     record[`${rule}:${syntax}:${state}`] = [nodes, offset];
   }
   public clear(position: number): void {
-    splice(this.memory, position, this.memory.length - position);
+    splice(this.memory, position + this.offset, this.memory.length - position + this.offset);
   }
 }
