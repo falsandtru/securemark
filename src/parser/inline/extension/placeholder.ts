abe75b40a8ddfbc74e45e4d2b527e3a02fe88187
falsandtru <falsandtru@users.noreply.github.com>
2023-02-01 10:26:04 +0900

    Refactoring

--- src/parser/inline/extension/placeholder.ts
+++ src/parser/inline/extension/placeholder.ts
@@ -7,12 +7,12 @@ import { startTight, trimBlankEnd } from '../../visibility';
 import { unshift } from 'spica/array';
 import { html, defrag } from 'typed-dom/dom';
 
-// Don't use the symbols already used: !#$%@&*+~=
+// Don't use the symbols already used: !#$%@&*+~=|
 
 // All syntax surrounded by square brackets shouldn't contain line breaks.
 
-export const placeholder: ExtensionParser.PlaceholderParser = lazy(() => validate(['[:', '[^'], surround(
-  str(/^\[[:^]/),
+export const placeholder: ExtensionParser.PlaceholderParser = lazy(() => validate('[', surround(
+  str(/^\[[:^|]/),
   syntax(Syntax.placeholder, 2, 1, State.none,
   startTight(trimBlankEnd(some(union([inline]), ']', [[']', 2]])))),
   str(']'), false,
