2f01b3546ffb42ad106f8b86b50a60755b47cb04
falsandtru <falsandtru@users.noreply.github.com>
2023-06-06 21:16:15 +0900

    Refactoring

--- src/parser/inline/extension/indexer.ts
+++ src/parser/inline/extension/indexer.ts
@@ -1,14 +1,12 @@
 import { ExtensionParser } from '../../inline';
-import { union, creation, state, focus, surround } from '../../../combinator';
+import { union, creation, focus, surround } from '../../../combinator';
 import { signature } from './index';
-import { State } from '../../context';
 import { html } from 'typed-dom/dom';
 
 export const indexer: ExtensionParser.IndexerParser = surround(
   /^\s+\[(?=\|\S)/,
-  state(State.index, false,
   union([
     signature,
     creation(focus(/^\|(?=\])/, () => [[html('span', { class: 'indexer', 'data-index': '' })], ''])),
-  ])),
+  ]),
   /^\]\s*$/);
