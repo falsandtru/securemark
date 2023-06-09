2edec3d5ca3bcbd6db94550ddd1399f5e69fa066
falsandtru <falsandtru@users.noreply.github.com>
2023-06-10 01:05:34 +0900

    Refactoring

--- src/parser/inline/bracket.ts
+++ src/parser/inline/bracket.ts
@@ -23,7 +23,6 @@ export const bracket: BracketParser = lazy(() => union([
   surround(str('{'), creation(syntax(Syntax.bracket, 2, State.none, some(inline, '}', [['}', 2]]))), str('}'), true,
     undefined,
     ([as, bs = []], rest) => [unshift(as, bs), rest]),
-  // Control media blinking in editing rather than control confusion of pairs of quote marks.
   surround(str('"'), creation(syntax(Syntax.none, 3, State.none, some(inline, '"', [['"', 3]]))), str('"'), true,
     undefined,
     ([as, bs = []], rest) => [unshift(as, bs), rest]),
