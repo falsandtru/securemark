f95a5bdb047b2b9c556cbdb8140ada848a4d3fe9
falsandtru <falsandtru@users.noreply.github.com>
2026-01-10 07:01:42 +0900

    Refactoring

--- src/parser/inline/htmlentity.ts
+++ src/parser/inline/htmlentity.ts
@@ -5,20 +5,20 @@ import { html } from 'typed-dom/dom';
 import { reduce } from 'spica/memoize';
 
 export const unsafehtmlentity: UnsafeHTMLEntityParser = creation(1, Recursion.ignore, validate('&', focus(
-  /^&[0-9A-Za-z]+;/,
+  /^&[0-9A-Za-z]{1,99};/,
   ({ source }) => [[parse(source) ?? `\x1B${source}`], ''])));
 
 export const htmlentity: HTMLEntityParser = fmap(
   union([unsafehtmlentity]),
-  ([test]) => [
-    test[0] === '\x1B'
+  ([text]) => [
+    text[0] === '\x1B'
       ? html('span', {
           class: 'invalid',
           'data-invalid-syntax': 'htmlentity',
           'data-invalid-type': 'syntax',
           'data-invalid-message': 'Invalid HTML entity',
-        }, test.slice(1))
-      : test,
+        }, text.slice(1))
+      : text,
   ]);
 
 const parse = reduce((el => (entity: string): string | undefined => {
