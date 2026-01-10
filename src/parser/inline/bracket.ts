4ea152cab9e08a79e2073c052d318f7daeeaeca8
falsandtru <falsandtru@users.noreply.github.com>
2026-01-10 10:36:26 +0900

    Refactoring

--- src/parser/inline/bracket.ts
+++ src/parser/inline/bracket.ts
@@ -6,14 +6,16 @@ import { State, Recursion, Backtrack } from '../context';
 import { unshift, push } from 'spica/array';
 import { html, defrag } from 'typed-dom/dom';
 
-const index = /^[0-9A-Za-z]+(?:(?:[.-]|, )[0-9A-Za-z]+)*/;
+const indexA = /^[0-9A-Za-z]+(?:(?:[.-]|, )[0-9A-Za-z]+)*/;
+const indexF = new RegExp(indexA.source.replace(', ', '[，、]')
+  .replace(/[09AZaz.]|\-(?!\w)/g, c => String.fromCodePoint(c.codePointAt(0)! + 0xFEE0)));
 
 export const bracket: BracketParser = lazy(() => union([
-  surround(str('('), creation(0, Recursion.bracket, syntax(2, State.none, str(index))), str(')')),
+  surround(str('('), creation(0, Recursion.bracket, syntax(2, State.none, str(indexA))), str(')')),
   surround(str('('), creation(0, Recursion.bracket, syntax(2, State.none, some(inline, ')', [[')', 2]]))), str(')'), true,
     ([as, bs = [], cs], rest) => [[html('span', { class: 'paren' }, defrag(push(unshift(as, bs), cs)))], rest],
     ([as, bs = []], rest) => [unshift(as, bs), rest], 3 | Backtrack.bracket),
-  surround(str('（'), creation(0, Recursion.bracket, syntax(2, State.none, str(new RegExp(index.source.replace(', ', '[，、]').replace(/[09AZaz.]|\-(?!\w)/g, c => c.trimStart() && String.fromCharCode(c.charCodeAt(0) + 0xFEE0)))))), str('）')),
+  surround(str('（'), creation(0, Recursion.bracket, syntax(2, State.none, str(indexF))), str('）')),
   surround(str('（'), creation(0, Recursion.bracket, syntax(2, State.none, some(inline, '）', [['）', 2]]))), str('）'), true,
     ([as, bs = [], cs], rest) => [[html('span', { class: 'paren' }, defrag(push(unshift(as, bs), cs)))], rest],
     ([as, bs = []], rest) => [unshift(as, bs), rest]),
