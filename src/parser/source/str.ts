07c3a784ebe33591bdf23358aa5366b8eeec9d83
falsandtru <falsandtru@users.noreply.github.com>
2026-01-19 13:34:26 +0900

    Refactoring

--- src/parser/source/str.ts
+++ src/parser/source/str.ts
@@ -5,6 +5,9 @@ import { consume } from '../../combinator';
 export function str(pattern: string | RegExp, not?: string): StrParser;
 export function str(pattern: string | RegExp, not?: string): Parser<string, Context<StrParser>, []> {
   assert(pattern);
+  const count = typeof pattern === 'object'
+    ? pattern.source.length > 9 || /[^^\\*+][*+]/.test(pattern.source)
+    : false;
   return typeof pattern === 'string'
     ? ({ source }) => {
         if (source === '') return;
@@ -16,7 +19,7 @@ export function str(pattern: string | RegExp, not?: string): Parser<string, Cont
     : ({ source, context }) => {
         if (source === '') return;
         const m = source.match(pattern);
-        m && consume(m[0].length, context);
+        count && m && consume(m[0].length, context);
         if (m && not && source.slice(m[0].length, m[0].length + not.length) === not) return;
         return m && m[0].length > 0
           ? [[m[0]], source.slice(m[0].length)]
