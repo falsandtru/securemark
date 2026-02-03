292346b71f10492078429f38e469cdaf34f827f0
falsandtru <falsandtru@users.noreply.github.com>
2026-02-03 12:24:28 +0900

    Refactoring

--- src/parser/inline/template.ts
+++ src/parser/inline/template.ts
@@ -11,7 +11,7 @@ export const template: TemplateParser = lazy(() => surround(
   '}}',
   true,
   ([, ns = []], rest) => [[html('span', { class: 'template' }, `{{${ns.join('')}}}`)], rest],
-  undefined, [3 | Backtrack.linedoublebracket, 1 | Backtrack.lineescbracket], Backtrack.bracket));
+  undefined, [3 | Backtrack.linedoublebracket, 1 | Backtrack.lineescbracket]));
 
 const bracket: TemplateParser.BracketParser = lazy(() => union([
   surround(str('('), recursion(Recursion.terminal, some(union([bracket, escsource]), ')')), str(')'), true,
