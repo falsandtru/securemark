c51c1fd8ded3408a4db0d7c3ea06e8ad9d96d0a6
falsandtru <falsandtru@users.noreply.github.com>
2026-02-08 06:11:03 +0900

    Refactoring

--- src/parser/source/str.ts
+++ src/parser/source/str.ts
@@ -19,11 +19,9 @@ export function str(pattern: string | RegExp, not?: string): Parser<string, Cont
     : ({ source, context }) => {
         if (source === '') return;
         const m = source.match(pattern);
-        count && m && consume(m[0].length, context);
-        if (m && not && source.slice(m[0].length, m[0].length + not.length) === not) return;
-        //assert(!m || m[0]);
-        return m
-          ? [[m[0]], source.slice(m[0].length)]
-          : undefined;
+        if (m === null) return;
+        count && consume(m[0].length, context);
+        if (not && source.slice(m[0].length, m[0].length + not.length) === not) return;
+        return [[m[0]], source.slice(m[0].length)];
       };
 }
