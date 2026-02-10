1cf18fd337e5afa14f16315360296106913a2cd3
falsandtru <falsandtru@users.noreply.github.com>
2026-02-11 06:52:19 +0900

    Refactoring

--- src/parser/inline/extension/indexer.ts
+++ src/parser/inline/extension/indexer.ts
@@ -3,6 +3,12 @@ import { union, focus, surround } from '../../../combinator';
 import { signature } from './index';
 import { html } from 'typed-dom/dom';
 
+// インデクスの重複解消は不要な重複を削除するのが最もよい。
+// 複合生成インデクスは参照と同期させることが困難であり
+// 複合生成インデクスを手動で同期させるより最初から重複のない
+// テキストまたはインデクスを付けて同期が必要な機会を減らすのが
+// 継続的編集において最も簡便となる。
+
 export const indexer: ExtensionParser.IndexerParser = surround(
   /^\s+\[(?=\|\S)/,
   union([
