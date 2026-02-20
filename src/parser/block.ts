580f36f9546b191b218e773b36c7797cd9deb5b2
falsandtru <falsandtru@users.noreply.github.com>
2026-02-21 01:37:23 +0900

    Refactoring

--- src/parser/block.ts
+++ src/parser/block.ts
@@ -42,7 +42,7 @@ export const block: BlockParser = reset(
   {
     resources: {
       // バックトラックのせいで文字数制限を受けないようにする。
-      clock: MAX_SEGMENT_SIZE * 5,
+      clock: MAX_SEGMENT_SIZE * 5 + 1,
       recursions: [
         10 || Recursion.block,
         20 || Recursion.blockquote,
