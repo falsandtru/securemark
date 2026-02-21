3bbf238c71f2993314536ffdb99ea37a9b2a7da1
falsandtru <falsandtru@users.noreply.github.com>
2026-02-21 10:29:47 +0900

    Refactoring

--- src/parser/source/line.ts
+++ src/parser/source/line.ts
@@ -1,6 +1,30 @@
 import { AnyLineParser, EmptyLineParser, ContentLineParser } from '../source';
-import { line, isBlank } from '../../combinator';
 
-export const anyline: AnyLineParser = line(() => [[]]);
-export const emptyline: EmptyLineParser = line(({ context: { source } }) => isBlank(source) ? [[]] : undefined);
-export const contentline: ContentLineParser = line(({ context: { source } }) => !isBlank(source) ? [[]] : undefined);
+export const anyline: AnyLineParser = input => {
+  const { context } = input;
+  const { source, position } = context;
+  context.position = source.indexOf('\n', position) + 1 || source.length;
+  return [[]];
+};
+const regEmptyline = /[^\S\n]*(?:$|\n)/y;
+export const emptyline: EmptyLineParser = input => {
+  const { context } = input;
+  const { source, position } = context;
+  regEmptyline.lastIndex = position;
+  regEmptyline.test(source);
+  const i = regEmptyline.lastIndex;
+  if (i === 0) return;
+  context.position = i;
+  return [[]];
+};
+const regContentline = /[^\S\n]*\S[^\n]*(?:$|\n)/y;
+export const contentline: ContentLineParser = input => {
+  const { context } = input;
+  const { source, position } = context;
+  regContentline.lastIndex = position;
+  regContentline.test(source);
+  const i = regContentline.lastIndex;
+  if (i === 0) return;
+  context.position = i;
+  return [[]];
+}
