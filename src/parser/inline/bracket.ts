638f26b67412d330af08d31d443aa7ea7bd25ccb
falsandtru <falsandtru@users.noreply.github.com>
2026-02-24 10:53:51 +0900

    Refactoring

--- src/parser/inline/bracket.ts
+++ src/parser/inline/bracket.ts
@@ -12,92 +12,121 @@ const indexF = new RegExp(indexA.source.replace(', ', '[，、]')
   .replace(/[09AZaz.]|\-(?!\w)/g, c => String.fromCodePoint(c.codePointAt(0)! + 0xFEE0)));
 
 export const bracket: BracketParser = lazy(() => union([
-  surround(
-    str('('),
-    precedence(1, recursion(Recursion.bracket, some(inline, ')', [[')', 1]]))),
-    str(')'),
-    true,
-    ([as, bs = [], cs], { source, position, range = 0 }) => {
-      const str = source.slice(position - range + 1, position - 1);
-      return indexA.test(str)
-        ? [[as[0], str, cs[0]]]
-        : [[html('span', { class: 'paren' }, defrag(push(unshift(as, bs), cs)))]];
-    },
-    ([as, bs = []]) => [unshift(as, bs)],
-    [2 | Backtrack.bracket]),
-  surround(
-    str('（'),
-    precedence(1, recursion(Recursion.bracket, some(inline, '）', [['）', 1]]))),
-    str('）'),
-    true,
-    ([as, bs = [], cs], { source, position, range = 0 }) => {
-      const str = source.slice(position - range + 1, position - 1);
-      return indexF.test(str)
-        ? [[as[0], str, cs[0]]]
-        : [[html('span', { class: 'paren' }, defrag(push(unshift(as, bs), cs)))]];
-    },
-    ([as, bs = []]) => [unshift(as, bs)]),
-  surround(
-    str('['),
-    precedence(1, recursion(Recursion.bracket, some(inline, ']', [[']', 1]]))),
-    str(']'),
-    true,
-    ([as, bs = [], cs], context) => {
-      if (context.state! & State.link) {
-        const { source, position, range = 0 } = context;
-        const head = position - range;
-        if (context.linebreak !== 0 || source[position] !== '{') {
+  input => {
+    const { context: { source, position } } = input;
+    switch (source[position]) {
+      case '(':
+        return p1(input);
+      case '（':
+        return p2(input);
+      case '[':
+        return s1(input);
+      case '［':
+        return s2(input);
+      case '{':
+        return c1(input);
+      case '｛':
+        return c2(input);
+      case '"':
+        return d1(input);
+    }
+  }
+])) as any;
+
+const p1 = lazy(() => surround(
+  str('('),
+  precedence(1, recursion(Recursion.bracket, some(inline, ')', [[')', 1]]))),
+  str(')'),
+  true,
+  ([as, bs = [], cs], { source, position, range = 0 }) => {
+    const str = source.slice(position - range + 1, position - 1);
+    return indexA.test(str)
+      ? [[as[0], str, cs[0]]]
+      : [[html('span', { class: 'paren' }, defrag(push(unshift(as, bs), cs)))]];
+  },
+  ([as, bs = []]) => [unshift(as, bs)],
+  [2 | Backtrack.bracket]));
+
+const p2 = lazy(() => surround(
+  str('（'),
+  precedence(1, recursion(Recursion.bracket, some(inline, '）', [['）', 1]]))),
+  str('）'),
+  true,
+  ([as, bs = [], cs], { source, position, range = 0 }) => {
+    const str = source.slice(position - range + 1, position - 1);
+    return indexF.test(str)
+      ? [[as[0], str, cs[0]]]
+      : [[html('span', { class: 'paren' }, defrag(push(unshift(as, bs), cs)))]];
+  },
+  ([as, bs = []]) => [unshift(as, bs)],
+  [2 | Backtrack.bracket]));
+
+const s1 = lazy(() => surround(
+  str('['),
+  precedence(1, recursion(Recursion.bracket, some(inline, ']', [[']', 1]]))),
+  str(']'),
+  true,
+  ([as, bs = [], cs], context) => {
+    if (context.state! & State.link) {
+      const { source, position, range = 0 } = context;
+      const head = position - range;
+      if (context.linebreak !== 0 || source[position] !== '{') {
+        setBacktrack(context, [2 | Backtrack.link], head);
+      }
+      else {
+        context.state! ^= State.link;
+        const result = !isBacktrack(context, [1 | Backtrack.link])
+          ? textlink({ context })
+          : undefined;
+        context.position = position;
+        if (!result) {
           setBacktrack(context, [2 | Backtrack.link], head);
         }
-        else {
-          context.state! ^= State.link;
-          const result = !isBacktrack(context, [1 | Backtrack.link])
-            ? textlink({ context })
-            : undefined;
-          context.position = position;
-          if (!result) {
-            setBacktrack(context, [2 | Backtrack.link], head);
-          }
-          context.state! ^= State.link;
-          context.range = range;
-        }
+        context.state! ^= State.link;
+        context.range = range;
       }
-      return [push(unshift(as, bs), cs)];
-    },
-    ([as, bs = []]) => [unshift(as, bs)],
-    [2 | Backtrack.bracket]),
-  surround(
-    str('［'),
-    precedence(1, recursion(Recursion.bracket, some(inline, '］', [['］', 1]]))),
-    str('］'),
-    true,
-    undefined,
-    ([as, bs = []]) => [unshift(as, bs)]),
-  surround(
-    str('{'),
-    precedence(1, recursion(Recursion.bracket, some(inline, '}', [['}', 1]]))),
-    str('}'),
-    true,
-    undefined,
-    ([as, bs = []]) => [unshift(as, bs)],
-    [2 | Backtrack.bracket]),
-  surround(
-    str('｛'),
-    precedence(1, recursion(Recursion.bracket, some(inline, '｝', [['｝', 1]]))),
-    str('｝'),
-    true,
-    undefined,
-    ([as, bs = []]) => [unshift(as, bs)]),
-  // 同一行内でしか閉じない以外括弧と同じ挙動
-  surround(
-    str('"'),
-    precedence(2, recursion(Recursion.bracket, some(inline, '"', [['"', 2, false]]))),
-    str('"'),
-    true,
-    ([as, bs = [], cs], context) =>
-      context.linebreak === 0
-        ? [push(unshift(as, bs), cs)]
-        : (context.position -= 1, [unshift(as, bs)]),
-    ([as, bs = []]) => [unshift(as, bs)],
-    [2 | Backtrack.bracket]),
-]));
+    }
+    return [push(unshift(as, bs), cs)];
+  },
+  ([as, bs = []]) => [unshift(as, bs)],
+  [2 | Backtrack.bracket]));
+
+const s2 = lazy(() => surround(
+  str('［'),
+  precedence(1, recursion(Recursion.bracket, some(inline, '］', [['］', 1]]))),
+  str('］'),
+  true,
+  undefined,
+  ([as, bs = []]) => [unshift(as, bs)],
+  [2 | Backtrack.bracket]));
+
+const c1 = lazy(() => surround(
+  str('{'),
+  precedence(1, recursion(Recursion.bracket, some(inline, '}', [['}', 1]]))),
+  str('}'),
+  true,
+  undefined,
+  ([as, bs = []]) => [unshift(as, bs)],
+  [2 | Backtrack.bracket]));
+
+const c2 = lazy(() => surround(
+  str('｛'),
+  precedence(1, recursion(Recursion.bracket, some(inline, '｝', [['｝', 1]]))),
+  str('｝'),
+  true,
+  undefined,
+  ([as, bs = []]) => [unshift(as, bs)],
+  [2 | Backtrack.bracket]));
+
+// 同一行内でしか閉じない以外括弧と同じ挙動
+const d1 = lazy(() => surround(
+  str('"'),
+  precedence(2, recursion(Recursion.bracket, some(inline, '"', [['"', 2, false]]))),
+  str('"'),
+  true,
+  ([as, bs = [], cs], context) =>
+    context.linebreak === 0
+      ? [push(unshift(as, bs), cs)]
+      : (context.position -= 1, [unshift(as, bs)]),
+  ([as, bs = []]) => [unshift(as, bs)],
+  [2 | Backtrack.bracket]));
