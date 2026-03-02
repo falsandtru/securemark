a54a0ffd4d2942c218b62cfaa6058e5344fd5854
falsandtru <falsandtru@users.noreply.github.com>
2026-03-02 21:35:51 +0900

    Refactoring

--- src/combinator/control/manipulation/lazy.ts
+++ src/combinator/control/manipulation/lazy.ts
@@ -4,7 +4,5 @@ export function lazy<P extends Parser<unknown>>(builder: () => P): P;
 export function lazy<N>(builder: () => Parser<N>): Parser<N> {
   let parser: Parser<N>;
   return input =>
-    parser !== undefined
-      ? parser(input)
-      : (parser = builder())(input);
+    (parser ??= builder())(input);
 }
