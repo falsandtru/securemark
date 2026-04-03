6724640cab051c65a48f79f51be036bf329c26c6
falsandtru <falsandtru@users.noreply.github.com>
2026-04-03 21:22:09 +0900

    Refactoring

--- src/parser/context.ts
+++ src/parser/context.ts
@@ -136,12 +136,7 @@ export const enum Backtrack {
 }
 
 export const enum Command {
-  Error = '\x07',
   Cancel = '\x18',
   Escape = '\x1B',
   Separator = '\x1F',
 }
-
-export const CmdRegExp = {
-  Error: /\x07/g,
-} as const;
